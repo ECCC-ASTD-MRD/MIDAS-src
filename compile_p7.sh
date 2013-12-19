0256bb246ce5d328244860eb0e5b6fa7d5cba921
Mark Buehner <mark.buehner@canada.ca>
2013-12-19 22:20:26 +0000

    Major modification to enable doing an ensemble of analyses within one execution. This required reorganizing some high-level routines (sugomobs and suinnov now called in cnt0 and not preproc) and adding a loop within cnt0.ftn. Commented out calculation of Jobs with effect of varqc in tovs_calc_jo.ftn90. Moved call to flaguvtofd_obsdat into update_burpfiles.ftn90. Moved calls for updating to burp files from postmin to cnt0 (for analysis mode only). New subroutine perturbobs.ftn90 for perturbing innovation vector (O-P), only active when doing more than one analysis.
    Moved contents of comgpsgb.cdk, namgpsgb.cdk and sugpsgb into the module modgpsztd_mod.
    
    Moved sutimeinterp into timeCoord_mod.ftn90. Also moved datestamp variable and related subroutines from minimization_mod to timeCoord_mod. Also moved time interpolation weights and related subroutines from columnData_mod into timeCoord_mod.
    
    Moved contents of comtovxt.cdk, extrap, lextrap and aextrap into new module tovs_extrap_mod.ftn90.
    
    Results are identical to the previous version.
    
    
    
    git-svn-id: svn://mrbsvn/assimilation/var/trunk@378 3ecdeb99-2462-4cc2-86f5-4e70ad797af3

--- compile_p7.sh
+++ compile_p7.sh
@@ -128,7 +128,7 @@ if [ $mode == full ] ; then
 
   echo "compiling low-level independent modules"
   SRC0="mathphysconstants_mod.ftn90 earthconstants_mod.ftn90 mpi_mod.ftn90 bufr_mod.ftn90 physicsfunctions_mod.ftn90"
-  SRC0="$SRC0 horizontalcoord_mod.ftn90 timecoord_mod.ftn90 verticalcoord_mod.ftn90"
+  SRC0="$SRC0 obsspacedata_mod.ftn90 horizontalcoord_mod.ftn90 timecoord_mod.ftn90 verticalcoord_mod.ftn90"
   s.compile $INCLUDES $COMPF -O -src $SRC0 > listing0a 2>&1
   grep fail listing0a
   if [ $? = "0" ] ; then exit ; fi
@@ -139,14 +139,13 @@ if [ $mode == full ] ; then
   grep fail listing0b
   if [ $? = "0" ] ; then exit ; fi
 
-  echo "compiling most of the new modules"svn 
+  echo "compiling most of the new modules"
   SRC1="controlvector_mod.ftn90 hir_chans_mod.ftn90 tovs_mod.ftn90 emissivities_mod.ftn90 fft_mod.ftn90"
-  SRC1="$SRC1 globalspectraltransform_mod.ftn90 obsspacedata_mod.ftn90 random_mod.ftn90 varnamelist_mod.ftn90"
+  SRC1="$SRC1 globalspectraltransform_mod.ftn90 random_mod.ftn90 varnamelist_mod.ftn90"
   SRC1="$SRC1 lamspectraltransform_mod.ftn90 columndata_mod.ftn90 gridstatevector_mod.ftn90"
   SRC1="$SRC1 bmatrixensemble_mod.ftn90 bmatrixhi_mod.ftn90 lambmatrixhi_mod.ftn90"
   SRC1="$SRC1 bmatrix_mod.ftn90 minimization_mod.ftn90"
-  SRC1="$SRC1 multi_ir_bgck_mod.ftn90 ozoneclim_mod.ftn90"
-  SRC1="$SRC1 filterobs_mod.ftn90"
+  SRC1="$SRC1 multi_ir_bgck_mod.ftn90 ozoneclim_mod.ftn90 tovs_extrap_mod.ftn90"
 
   s.compile $INCLUDES $COMPF -O -src $SRC1 > listing1 2>&1
   grep fail listing1
@@ -162,6 +161,7 @@ if [ $mode == full ] ; then
   SRC2="modgps00base.cdk90 modgps01ctmath.cdk90 modgps01ctphys.cdk90 modgps02wgs84const.cdk90 modgps02wgs84grav.cdk90 modgps03diff.cdk90 modgps04profile.cdk90"
   SRC2="$SRC2 modgps05refstruct.cdk90 modgps07geostruct.cdk90 modgps08refop.cdk90 modgps09bend.cdk90 modgpsro_mod.ftn90 modgps04profilezd.cdk90"
   SRC2="$SRC2 modgps08ztdop.cdk90 modgpsztd_mod.ftn90"
+  SRC2="$SRC2 filterobs_mod.ftn90"
   s.compile $INCLUDES $COMPF -O -src $SRC2 > listing2 2>&1
   grep fail listing2
   if [ $? = "0" ] ; then exit ; fi
