6055ec25c3593d50b65d77dadb627e80de1a0806
Mark Buehner <mark.buehner@canada.ca>
2014-03-18 20:10:12 +0000

    Modified bmatrixhi_mod.ftn90 so that the square-root of B is now a true square square-root. Also removed the old copy of eigen decomposition subroutine that was in the modulopt subdirectory. This should make the analysis system much more robust to small changes in how the eigenvalue decomposition of B is computed (like when ESSL dynamic library changed). These changes are equivalent to those in revisions 348 and 349 of the trunk, which were then reverted shortly afterwards.
    NOTE!!!: these changes mean that the Hessian (pm1q) files computed with previous revisions can no longer be used with this and subsequent revisions. That is, the Hessian must be recomputed before launching the first real data assimilation experiment with this or future revisions.
    
    
    
    git-svn-id: svn://mrbsvn/assimilation/var/trunk@406 3ecdeb99-2462-4cc2-86f5-4e70ad797af3

--- compile_p7.sh
+++ compile_p7.sh
@@ -120,8 +120,8 @@ if [ $mode == full ] ; then
   rm -f enkf_pturb.ftn
 
   # Compile the subroutines...
-  echo "compiling modulopt (n1qn3) [ALSO DSYEV WHICH SHOULD NOT BE HERE!]"
-  SRC0="dcube.ftn ddd.ftn ddds.ftn dsyev.ftn dystbl.ftn mupdts.ftn n1qn3.ftn n1qn3a.ftn nlis0.ftn"
+  echo "compiling modulopt (n1qn3)"
+  SRC0="dcube.ftn ddd.ftn ddds.ftn dystbl.ftn mupdts.ftn n1qn3.ftn n1qn3a.ftn nlis0.ftn"
   s.compile $INCLUDES $COMPF_NOC -O -src $SRC0 > listing0 2>&1
   grep fail listing0
   if [ $? = "0" ] ; then exit ; fi
