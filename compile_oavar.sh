a06fba7b1e4362095a36054ded42666efa7e187f
Ervig Lapalme <ervig.lapalme@canada.ca>
2015-01-07 16:33:18 +0000

    The compiling script learns a new environment variable 'COMPILE_OAVAR_REMOVE_DEBUG_OPTIONS' which when is equal to 'yes', it does not add options '-debug DEBUG -optf=-C'. We also did some improvements in the information provided to the user when the compilation aborts.
    git-svn-id: svn://mrbsvn/assimilation/var/trunk@522 3ecdeb99-2462-4cc2-86f5-4e70ad797af3

--- compile_oavar.sh
+++ compile_oavar.sh
@@ -70,7 +70,7 @@ echo "-----------------------"
 echo "Revision number='$revnum' '$revpath'"
 echo "-----------------------"
 echo " "
-compiledir=$PWD
+compiledir=${PWD}
 
 #----------------------------------------------------------------
 #  Set up dependent librarys and tools. 
@@ -117,15 +117,20 @@ else
     echo "This platform 'BASE_ARCH=${BASE_ARCH}' is not supported.  Only 'AIX-powerpc7' and 'Linux_x86-64' are."
     exit 1
 fi
-LIBRMN="rmn"
-COMPF_NOC="-openmp $MPIKEY "
-COMPF="$COMPF_NOC  -debug DEBUG -optf=-C"
+LIBRMN=rmn
+COMPF_NOC="-openmp ${MPIKEY}"
+if [ "${COMPILE_OAVAR_REMOVE_DEBUG_OPTIONS}" = yes ]; then
+    COMPF=${COMPF_NOC}
+else
+    COMPF="${COMPF_NOC} -debug DEBUG -optf=-C"
+fi
+
 if [ "${mode}" == full ] ; then
 
   rm -f *.o *.mod *.cdk* *.h *.ftn* *.f *.f90
 
   # Create a local copy of the source code
-  sed "s!XXXXX!${revnum} ${revpath}!g" ${trunkdir}/toplevelcontrol_mod.ftn90_template > toplevelcontrol_mod.ftn90
+  #sed "s!XXXXX!${revnum} ${revpath}!g" ${trunkdir}/toplevelcontrol_mod.ftn90_template > toplevelcontrol_mod.ftn90
 
   cd ${trunkdir};          ls -1F | grep -v '/' | grep -v "*" | grep -v "@" | cpio -pl $compiledir ; cd $compiledir
   cd ${trunkdir}/bgcheck;  ls -1F | grep -v '/' | grep -v "*" | cpio -pl $compiledir ; cd $compiledir
@@ -141,30 +146,43 @@ if [ "${mode}" == full ] ; then
 
   # Compile the subroutines...
   echo "compiling low-level independent modules"
+  echo "If aborting, check in ${PWD}/listing1"
   SRC0="toplevelcontrol_mod.ftn90"
   SRC0="$SRC0 mathphysconstants_mod.ftn90 earthconstants_mod.ftn90 mpi_mod.ftn90 mpivar_mod.ftn90 bufr_mod.ftn90 codtyp_mod.ftn90"
   SRC0="$SRC0 physicsfunctions_mod.ftn90 obsspacedata_mod.ftn90 horizontalcoord_mod.ftn90 timecoord_mod.ftn90 verticalcoord_mod.ftn90"
   s.compile $COMPF -O ${FOPTMIZ} -src $SRC0 > listing1 2>&1
   status=1
   grep fail listing1 || status=0
-  if [ "${status}" -ne 0 ] ; then exit 1; fi
+  if [ "${status}" -ne 0 ] ; then
+      echo "Compilation aborted: check in ${PWD}/listing1"
+      exit 1
+  fi
 
   echo "compiling modulopt (n1qn3)"
+  echo "If aborting, check in ${PWD}/listing0"
   SRC0="dcube.ftn ddd.ftn ddds.ftn dystbl.ftn mupdts.ftn n1qn3.ftn n1qn3a.ftn nlis0.ftn"
   s.compile $COMPF_NOC  -O ${FOPTMIZ} -src $SRC0 > listing0 2>&1
   status=1
   grep fail listing0 || status=0
-  if [ "${status}" -ne 0 ] ; then exit 1; fi
+  if [ "${status}" -ne 0 ]; then
+      echo "Compilation aborted: check in ${PWD}/listing0"
+      exit 1
+  fi
   rm -f $SRC0
 
   echo "compiling analysis grid modules"
+  echo "If aborting, check in ${PWD}/listing2"
   SRC0="gaussgrid_mod.ftn90 windrotation_mod.ftn90 lamanalysisgrid_mod.ftn90"
   s.compile $COMPF  -O ${FOPTMIZ} -src $SRC0 > listing2 2>&1
   status=1
   grep fail listing2 || status=0
-  if [ "${status}" -ne 0 ] ; then exit 1; fi
+  if [ "${status}" -ne 0 ]; then
+      echo "Compilation aborted: check in ${PWD}/listing2"
+      exit 1
+  fi
 
   echo "compiling most of the new modules"
+  echo "If aborting, check in ${PWD}/listing3"
   SRC1="controlvector_mod.ftn90 rmatrix_mod.ftn90 hir_chans_mod.ftn90 tovs_nl_mod.ftn90 tovs_lin_mod.ftn90 varnamelist_mod.ftn90 columndata_mod.ftn90 multi_ir_bgck_mod.ftn90"
   SRC1="$SRC1 emissivities_mod.ftn90 fft_mod.ftn90 globalspectraltransform_mod.ftn90"
   SRC1="$SRC1 lamspectraltransform_mod.ftn90  gridstatevector_mod.ftn90"
@@ -176,32 +194,48 @@ if [ "${mode}" == full ] ; then
   s.compile $COMPF  -O ${FOPTMIZ} -src $SRC1 > listing3 2>&1
   status=1
   grep fail listing3 || status=0
-  if [ "${status}" -ne 0 ] ; then exit 1; fi
+  if [ "${status}" -ne 0 ]; then
+      echo "Compilation aborted: check in ${PWD}/listing3"
+      exit 1
+  fi
 
   echo "compiling burp_read module"
+  echo "If aborting, check in ${PWD}/listing4"
   SRC1="burp_read_mod.ftn90 burp_functions.ftn90 selectb.ftn90 update_burpfiles.ftn90"
   s.compile $COMPF  -O ${FOPTMIZ} -src $SRC1 > listing4 2>&1
   status=1
   grep fail listing4 || status=0
-  if [ "${status}" -ne 0 ] ; then exit 1; fi
+  if [ "${status}" -ne 0 ]; then
+      echo "Compilation aborted: check in ${PWD}/listing4"
+      exit 1
+  fi
   
   echo "compiling the GPS modules (cdk90)..."
+  echo "If aborting, check in ${PWD}/listing5"
   SRC2="modgps00base.cdk90 modgps01ctmath.cdk90 modgps01ctphys.cdk90 modgps02wgs84const.cdk90 modgps02wgs84grav.cdk90 modgps03diff.cdk90 modgps04profile.cdk90"
   SRC2="$SRC2 modgps05refstruct.cdk90 modgps07geostruct.cdk90 modgps08refop.cdk90 modgps09bend.cdk90 modgps04profilezd.cdk90"
   SRC2="$SRC2 modgps08ztdop.cdk90"
   s.compile $COMPF  -O ${FOPTMIZ} -src $SRC2 > listing5 2>&1
   status=1
   grep fail listing5 || status=0
-  if [ "${status}" -ne 0 ] ; then exit 1; fi
+  if [ "${status}" -ne 0 ] ; then
+      echo "Compilation aborted: check in ${PWD}/listing5"
+      exit 1
+  fi
 
   echo "compiling some more modules..."
+  echo "If aborting, check in ${PWD}/listing6"
   SRC2="modgpsro_mod.ftn90 modgpsztd_mod.ftn90 filterobs_mod.ftn90 writeincrement_mod.ftn90 obsoperators_mod.ftn90"
   s.compile $COMPF  -O ${FOPTMIZ} -src $SRC2 > listing6 2>&1
   status=1
   grep fail listing6 || status=0
-  if [ "${status}" -ne 0 ] ; then exit 1; fi
+  if [ "${status}" -ne 0 ]; then
+      echo "Compilation aborted: check in ${PWD}/listing6"
+      exit 1
+  fi
 
   echo "compiling remaining ftn ftn90..."
+  echo "If aborting, check in ${PWD}/listing7"
   filelist=""
   for i in *.ftn *.ftn90 ; do
       if [[ "${i}" != *_mod.ftn* ]]; then
@@ -211,14 +245,25 @@ if [ "${mode}" == full ] ; then
   s.compile $COMPF  -O ${FOPTMIZ} -src $filelist > listing7 2>&1
   status=1
   grep fail listing7 || status=0
-  if [ "${status}" -ne 0 ] ; then exit 1; fi
+  if [ "${status}" -ne 0 ]; then
+      echo "Compilation aborted: check in ${PWD}/listing7"
+      exit 1
+  fi
 
   echo "building the executable..."
+  echo "If aborting, check in ${PWD}/listing8"
   s.compile $COMPF  -O ${FOPTMIZ} -libappl $LIBAPPL $LIBEXTRA -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o ${varabs}.Abs > listing8 2>&1
+  status=1
+  grep fail listing8 || status=0
+  if [ "${status}" -ne 0 ]; then
+      echo "Compilation aborted: check in ${PWD}/listing8"
+      exit 1
+  fi
+
   status=1
   grep -i ERROR listing? || status=0
   if [ "${status}" -ne 0 ] ; then
-      echo "ERROR found: STOP"
+      echo "ERROR found: STOP; check listing in ${PWD}"
       exit 1
   fi
 
@@ -229,7 +274,7 @@ if [ "${mode}" == full ] ; then
 
 elif [ "${mode}" == abs ] ; then
 
-  rm -f 3dvar_p7.abs$ABSTAG
+  rm -f ${varabs}.Abs
 
   echo
   echo "building the executable..."
@@ -237,17 +282,15 @@ elif [ "${mode}" == abs ] ; then
   s.compile $COMPF  -O ${FOPTMIZ} -libappl $LIBAPPL $LIBEXTRA -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o ${varabs}.Abs
 
 else
-
-  if [ -f $trunkdir/$mode ] ; then
-    file=`basename $mode`
-    rm -f $file
-    cp $trunkdir/$mode .
-    s.compile $COMPF  -O ${FOPTMIZ} -src $file
-  else
-    echo "File $trunkdir/$mode does NOT exist. Stop"
-    exit 1
-  fi
-
+    if [ -f $trunkdir/$mode ] ; then
+	file=`basename $mode`
+	rm -f $file
+	cp $trunkdir/$mode .
+	s.compile $COMPF  -O ${FOPTMIZ} -src $file
+    else
+	echo "File $trunkdir/$mode does NOT exist. Stop"
+	exit 1
+    fi
 fi
 
 echo "FINISHED COMPILATION AT:"
