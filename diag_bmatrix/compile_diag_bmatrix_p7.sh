708109d883a88fe93288aa519365cf2dcdc1bdd0
Jean-Francois Caron <jean-francois.caron@canada.ca>
2015-05-14 20:55:32 +0000

    Brought back to life diag_bmatrix + introduced some changes to gridstatevector made by Mark B.
    git-svn-id: svn://mrbsvn/assimilation/var/trunk@590 3ecdeb99-2462-4cc2-86f5-4e70ad797af3

--- diag_bmatrix/compile_diag_bmatrix_p7.sh
+++ diag_bmatrix/compile_diag_bmatrix_p7.sh
@@ -1,120 +1,178 @@
 #!/bin/ksh
-nompi=$1
-if [ "$nompi" = "NOMPI" -o "$nompi" = "nompi" ] 
-then
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  echo "!!Compiling for a NON-MPI executable!!"
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  MPILIB="rpn_commstubs_40007 rpn_comm_40007"
-  MPIKEY=""
-  ABSTAG="_NOMPI"
-else
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  echo "!!Compiling for an MPI executable!!"
-  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  MPILIB="rpn_comm_40007"
-  MPIKEY="-mpi"
-  ABSTAG=""
+
+mode=$1
+
+if [ "$mode" == "" ] ; then
+  echo " "
+  echo "------------------------------------------------------- "
+  echo "WARNING: no compilation mode specified, assuming 'full'"
+  echo "------------------------------------------------------- "
+  echo " "
+  mode=full
 fi
 
-trunkdir=${PWD}/../
+if [ $mode == full ] ; then
+  echo
+  echo "----------------------------------------"
+  echo " >>> Full Compilation"
+  echo "----------------------------------------"
+  echo
+elif [ $mode == abs ] ; then
+  echo
+  echo "----------------------------------------"
+  echo " >>> Building the Executable only"
+  echo "----------------------------------------"
+  echo
+else
+  echo
+  echo "----------------------------------------"
+  echo " >>> Compiling only routine : $1"
+  echo "----------------------------------------"
+  echo
+fi
 
-# automatically set the global revision number in comct0.cdk by
-# replacing the string XXXXX with the actual revision number
-revnum=`ssh alef "cd $trunkdir ; svnversion"`
-cat ../comct0_template.cdk |sed "s/XXXXX/${revnum}/g" > ../comct0.cdk
+echo " Compiling for an MPI executable"
+echo ""
+MPILIBDIR="-libpath /users/dor/arma/gr3/userlibs/AIX-powerpc7/xlf13" # JFC : Mesure temporaire pour avoir acces a
+MPILIB="rpn_comm_adj_halo8"                                          #       la S-R RPN_COMM_adj_halo8 de M. Valin
+MPIKEY="-mpi"
+ABSTAG=""
 
-compiledir="../../compiledir"
+diag_bmatrixdir=$PWD
+trunkdir=$PWD/../
+
+## for s.compile
+echo "loading hpcs/201402/02/base"
+. ssmuse-sh -d hpcs/201402/02/base
+
+## for the compiler
+echo "loading compiler hpcs/ext/xlf_13.1.0.10"
+. ssmuse-sh -d hpcs/ext/xlf_13.1.0.10
+
+## for rmn, lapack_3.4.0, rpncomm
+echo "loading rpn/libs/15.2"
+. ssmuse-sh -d rpn/libs/15.2
+
+## for 'vgrid'
+echo "loading cmdn/vgrid/5.3.2/${COMP_ARCH}"
+. ssmuse-sh -d cmdn/vgrid/5.3.2/${COMP_ARCH}
+
+## for 'burplib'
+echo "loading cmda/base/201411/01/${COMP_ARCH}"
+. ssmuse-sh -d cmda/base/201411/01/${COMP_ARCH}
+
+## For RTTOV 10v1 package... 
+echo "loading arma/rttov/10v1"
+. ssmuse-sh -d arma/rttov/10v1
+
+## For hpcsperf needed for TMG timings
+. ssmuse-sh -d hpcs/exp/aspgjdm/perftools
+
+VAR3D_VERSION="11.2.1"
+LIBAPPL="rttov10.2.0_coef_io rttov10.2.0_main rttov10.2.0_other burp_module descrip $MPILIB"
+
+LIBSYS="essl mass"
+LIBRMN="rmn"
+LIBEXTRA="hpcsperf lapack-3.4.0"
+COMPF_NOC="-openmp $MPIKEY -O"
+#COMPF="$COMPF_NOC"
+COMPF="$COMPF_NOC -debug DEBUG -optf=-C "
+
+# Create and Move to compilation directory
+compiledir="../../compiledir_diag_bmatrix"
 echo "COMPILEDIR IS SET TO: $compiledir"
 mkdir -p $compiledir
 cd $compiledir
-rm -f *.o *.f *.f90 *.mod
 
 compiledir=$PWD
 
-. s.ssmuse.dot Xlf13.108
-. s.ssmuse.dot rmnlib-dev
-. s.ssmuse.dot devtools
-. /ssm/net/hpcs/shortcuts/ssmuse_ssm_v10.sh
-. s.ssmuse.dot CMDN/vgrid/3.4.0
-. s.ssmuse.dot rpn_comm
+if [ $mode == full ] ; then
 
+  rm -f *.o *.mod *.cdk* *.h *.ftn* *.f *.f90
 
-VAR3D_VERSION="11.2.1"
-LIBAPPL="descrip $MPILIB "
-LIBSYS="lapack blas mass"
-LIBRMN="rmn_013_rc2"
-LIBEXTRA="rtools hpm_r"
-MODRTTOV="RTTOV8.7"
-MODBURP="BURP1.3"
-DEFINE="-DNEC=nec -DIBM=ibm"
-ABI="_multi"
-COMPF="-openmp $MPIKEY -debug DEBUG -optf=-C"
-FCOMPF="-options_comp"
+  # Create a local copy of the source code
+  trunkfiles="mpi_mod.ftn90 mpivar_mod.ftn90 abort.ftn physicsfunctions_mod.ftn90 controlvector_mod.ftn90 \
+            gaussgrid_mod.ftn90 fft_mod.ftn90 globalspectraltransform_mod.ftn90 lamanalysisgrid_mod.ftn90 \
+            gridstatevector_mod.ftn90 maincompileswitch.inc varnamelist_mod.ftn90 lambmatrixhi_mod.ftn90 \
+            utils_3dvar.ftn lamspectraltransform_mod.ftn90 shared/obsspacedata_mod.ftn90 getstamplist.ftn90 \
+            verticalcoord_mod.ftn90 horizontalcoord_mod.ftn90 dsyev2.ftn timecoord_mod.ftn90 \
+            columndata_mod.ftn90 bmatrixensemble_mod.ftn90 bmatrixhi_mod.ftn90 bmatrix_mod.ftn90 \
+            matsqrt.ftn getfldprm2.ftn gasdev.ftn getstepobsindex.ftn90"
 
-LIBPATH2="./ $LIBPATH"
+  cd ${trunkdir}
+  cp -f diag_bmatrix/diag_bmatrix.ftn90 ${compiledir}
+  cp -f ${trunkfiles} ${compiledir}
+  cd ${trunkdir}/shared; ls -1F | grep -v '/' | grep -v "*" | cpio -pl ${compiledir}
+  cd ${compiledir}
 
-echo "LIBPATH2="
-echo $LIBPATH2
-echo "INCLUDES="
-echo $INCLUDES
+  echo "STARTING COMPILATION AT:" 
+  date
 
-trunkfiles="abort.ftn bmatrix_mod.ftn90 bmatrixensemble_mod.ftn90 bmatrixhi_mod.ftn90 \
-            columndata_mod.ftn90 comfilt.cdk controlvector_mod.ftn90 \
-            gasdev.ftn gaussgrid_mod.ftn90 gdout2.ftn getfldprm2.ftn \
-            getstamplist.ftn90 gridstatevector_mod.ftn90 maincompileswitch.inc \
-            matsqrt.ftn mpi_mod.ftn90 utils_3dvar.ftn \
-            varnamelist_mod.ftn90 varout.ftn verticalcoord_mod.ftn90 comct0.cdk"
+  # Compile the subroutines...
+  echo "compiling low-level independent modules"
+  SRC0="mathphysconstants_mod.ftn90 earthconstants_mod.ftn90 mpi_mod.ftn90 mpivar_mod.ftn90"
+  SRC0="$SRC0 bufr_mod.ftn90 physicsfunctions_mod.ftn90 horizontalcoord_mod.ftn90 obsspacedata_mod.ftn90"
+  s.compile $COMPF -src $SRC0 > listing0a 2>&1
+  grep fail listing0a
+  if [ $? = "0" ] ; then exit ; fi
 
-moduloptfiles="dsyev.ftn"
+  echo "compiling analysis grid modules"
+  SRC0="gaussgrid_mod.ftn90 lamanalysisgrid_mod.ftn90"
+  s.compile $COMPF -src $SRC0 > listing0b 2>&1
+  grep fail listing0b
+  if [ $? = "0" ] ; then exit ; fi
 
-cd ${trunkdir}
-cp -f diag_bmatrix/diag_bmatrix.ftn90 ${compiledir}
-cp -f ${trunkfiles} ${compiledir}
-cp -f modulopt/${moduloptfiles} ${compiledir}
-cd ${trunkdir}/shared; ls -1F | grep -v '/' | grep -v "*" | cpio -pl ${compiledir}
-cd ${compiledir}
+  echo "compiling most of the new modules"
+  SRC1="controlvector_mod.ftn90 fft_mod.ftn90 varnamelist_mod.ftn90 timecoord_mod.ftn90"
+  SRC1="$SRC1 globalspectraltransform_mod.ftn90 verticalcoord_mod.ftn90"
+  SRC1="$SRC1 lamspectraltransform_mod.ftn90 gridstatevector_mod.ftn90"
+  SRC1="$SRC1 columndata_mod.ftn90 lambmatrixhi_mod.ftn90"
+  SRC1="$SRC1 bmatrixensemble_mod.ftn90 bmatrixhi_mod.ftn90"
+  SRC1="$SRC1 bmatrix_mod.ftn90"
+  s.compile $COMPF -src $SRC1 > listing1 2>&1
+  grep fail listing1
+  if [ $? = "0" ] ; then exit ; fi
 
-rm -f *.ftn~ *.ftn90~
+  echo "compiling remaining ftn ftn90..."
+  filelist=""
+  for i in *.ftn *.ftn90 ; do
+    xx=`echo $i |grep -v _mod.ftn` 
+    filelist="$filelist $xx"
+  done
+  s.compile $COMPF -src $filelist > listing4 2>&1
+  grep fail listing4
+  if [ $? = "0" ] ; then exit ; fi
 
-echo "STARTING COMPILATION AT:"
-date
+  echo "building the executable..."
+  s.compile $COMPF ${MPILIBDIR} -libappl $LIBAPPL $LIBEXTRA -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o diag_bmatrix_p7.abs$ABSTAG > listing5 2>&1
 
-echo "compiling low-level independent modules"
-SRC0="mathphysconstants_mod.ftn90 earthconstants_mod.ftn90 mpi_mod.ftn90 bufr_mod.ftn90"
-s.compile $INCLUDES $COMPF -O -src $SRC0 > listing0 2>&1
-grep fail listing0
-if [ $? = "0" ] ; then exit ; fi
+  grep -i ERROR listing?
+  if [ $? = "0" ] ; then exit; echo "ERROR found: STOP" ; fi
 
-echo "compiling most of the new modules"
-SRC1="controlvector_mod.ftn90 fft_mod.ftn90"
-SRC1="$SRC1 gaussgrid_mod.ftn90 globalspectraltransform_mod.ftn90 obsspacedata_mod.ftn90 random_mod.ftn90 varnamelist_mod.ftn90 verticalcoord_mod.ftn90"
-SRC1="$SRC1 columndata_mod.ftn90 gridstatevector_mod.ftn90"
-SRC1="$SRC1 bmatrixensemble_mod.ftn90 bmatrixhi_mod.ftn90"
-SRC1="$SRC1 bmatrix_mod.ftn90"
-s.compile $INCLUDES $COMPF -O -src $SRC1 > listing1 2>&1
-grep fail listing1
-if [ $? = "0" ] ; then exit ; fi
+  rm -f *.ftn* 
 
-echo "compiling remaining ftn ftn90..."
-filelist=""
-for i in *.ftn *.ftn90
-do
-  xx=`echo $i |grep -v _mod.ftn` 
-  filelist="$filelist $xx"
-done
-s.compile $INCLUDES $COMPF -O -src $filelist > listing4 2>&1
-grep fail listing4
-if [ $? = "0" ] ; then exit ; fi
+  echo "FINISHED COMPILATION AT:"
+  date
 
-echo "building the executable..."
-s.compile -O -abi $ABI $COMPF $INCLUDES -libpriv -libpath $LIBPATH2 -libappl $LIBAPPL $LIBEXTRA -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o diag_bmatrix_p7.abs$ABSTAG > listing5 2>&1
+elif [ $mode == abs ] ; then
 
-grep -i ERROR listing?
-if [ $? = "0" ] ; then exit; echo "ERROR found: STOP" ; fi
+  rm -f diag_bmatrix_p7.abs$ABSTAG
 
-rm -f *.ftn* *.cdk* *.h
+  echo
+  echo "building the executable..."
+  echo
+  s.compile $COMPF ${MPILIBDIR} -libappl $LIBAPPL $LIBEXTRA -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o diag_bmatrix_p7.abs$ABSTAG
 
-echo "FINISHED COMPILATION AT:"
-date
+else
 
+  if [ -f $diag_bmatrixdir/$mode ] ; then
+    file=`basename $mode`
+    rm -f $file
+    cp $diag_bmatrixdir/$mode .
+    s.compile $COMPF -src $file
+  else
+    echo "File $diag_bmatrixdir/$mode does NOT exist. Stop"
+    exit 1
+  fi
+
+fi
