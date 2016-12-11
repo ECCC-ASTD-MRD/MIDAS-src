04a7b6cbbc28313ac58fffb3f4324c148c8d19fc
Jean-Francois Caron <jean-francois.caron@ec.gc.ca>
2016-12-11 17:33:01 -0500

    Towards a module-only code. Part 2.
    Farewell to the oda_* routines...

--- compile_oavar.sh
+++ compile_oavar.sh
@@ -203,7 +203,7 @@ if [ "${mode}" == full ] ; then
   SRC0="$SRC0 mathphysconstants_mod.ftn90 earthconstants_mod.ftn90 mpi_mod.ftn90 mpivar_mod.ftn90 bufr_mod.ftn90 codtyp_mod.ftn90"
   SRC0="$SRC0 physicsfunctions_mod.ftn90 obsspacedata_mod.ftn90 localizationfunction_mod.ftn90"
   SRC0="$SRC0 horizontalcoord_mod.ftn90 timecoord_mod.ftn90 verticalcoord_mod.ftn90"
-  SRC0="$SRC0 lqtoes_mod.ftn90 intavg_mod.ftn90 filterresponsefunction_mod.ftn90"
+  SRC0="$SRC0 lqtoes_mod.ftn90 intavg_mod.ftn90 spectraltransform_mod.ftn90"
   s.compile $COMPF -O ${FOPTMIZ} -src $SRC0 > listing1 2>&1
   status=1
   grep fail listing1 || status=0
@@ -242,9 +242,8 @@ if [ "${mode}" == full ] ; then
   SRC1="$SRC1 emissivities_mod.ftn90 globalspectraltransform_mod.ftn90 tt2phi_mod.ftn90"
   SRC1="$SRC1 lamspectraltransform_mod.ftn90 gridstatevector_mod.ftn90 statetocolumn_mod.ftn90 variabletransforms_mod.ftn90"
   SRC1="$SRC1 bmatrixensemble_mod.ftn90 bmatrixhi_mod.ftn90 lambmatrixhi_mod.ftn90"
-  SRC1="$SRC1 bmatrix_mod.ftn90"
+  SRC1="$SRC1 bmatrix_mod.ftn90 residual_mod.ftn90 costfunction_mod.ftn90"
   SRC1="$SRC1 ozoneclim_mod.ftn90 tovs_extrap_mod.ftn90"
-  SRC1="$SRC1 obsspacediag_mod.ftn90 obserrors_mod.ftn90"
 
   s.compile $COMPF  -O ${FOPTMIZ} -src $SRC1 > listing3 2>&1
   status=1
@@ -278,8 +277,8 @@ if [ "${mode}" == full ] ; then
 
   echo "... > Compiling some more modules..."
   echo "...   if aborting, check in ${PWD}/listing6"
-  SRC2="varqc_mod.ftn90 obsfilter_mod.ftn90 obsoperators_mod.ftn90"
-  SRC2="$SRC2 minimization_mod.ftn90 burpfiles_mod.ftn90 innovation_mod.ftn90"
+  SRC2="obserrors_mod.ftn90 varqc_mod.ftn90 obsfilter_mod.ftn90 obsoperators_mod.ftn90 obsspacediag_mod.ftn90 burpfiles_mod.ftn90 innovation_mod.ftn90"
+  SRC2="$SRC2 minimization_mod.ftn90"
   s.compile $COMPF  -O ${FOPTMIZ} -src $SRC2 > listing6 2>&1
   status=1
   grep fail listing6 || status=0
