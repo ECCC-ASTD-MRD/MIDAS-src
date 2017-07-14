e252ae0e3b2318cc06b504d49aab76173a87e810
Mark Buehner <mark.buehner@canada.ca>
2017-07-14 21:06:14 +0000

    Main corrections to randompert for XC40
    Updated compile script and main program to work
    correctly on XC40 and PPP. Added a new namelist
    variable WRITE_MPI to control whether or not to
    write the perturbation files as complete files or
    files that are separated over all mpi tasks.
    
    The main oavar compile script was also split into
    two parts, with all of the general "environment"
    setup put in compile_commons.sh which is included
    in the main compile scripts for both oavar and
    randompert.
    
    Randompert was tested on both hare and ppp2. The
    mpi writing is much faster than writing complete
    files on hare, but doesn't seem to help much on
    ppp2, for which the cost of writing dominates.

--- compile_oavar.sh
+++ compile_oavar.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Revisions:
-#            Mike Sitwell, Y.J. Rochon, and Ping Du (as of Dec 2014)
-#            - The following are changes/additions for chemical constituents.
-#            - Added compilation of bmatrixchem_mod.ftn90
-#            - Addition of obssubspacedata_mod.ftn90 and chem_*_mod.ftn90 
-
 set -e
 
 mode=$1
 nompi=$2
+
+. ./compile_commons.sh
 
 echo "..."
 echo "...             |==================================|"
@@ -37,34 +33,15 @@ if [ "$nompi" = "NOMPI" -o "$nompi" = "nompi" ] ; then
   echo "..."
   echo "... > Compiling for a NON-MPI executable"
   MPILIB="rpn_commstubs rpn_comm"
-  MPIKEY=""
   ABSTAG="_nompi"
 else
   echo "..."
   echo "... > Compiling for an MPI executable"
   MPILIB="rpn_comm"
-  MPIKEY="-mpi"
   ABSTAG=""
 fi
 
 trunkdir=$PWD
-
-if [ "${ORDENV_PLAT}" = sles-11-haswell-64-xc40 ];then
-    echo "... Switching ORDENV_PLAT from '${ORDENV_PLAT}' to 'sles-11-broadwell-64-xc40'"
-    . r.env.dot --arch sles-11-broadwell-64-xc40
-    echo ORDENV_PLAT=${ORDENV_PLAT}
-fi
-
-if [ "${ORDENV_PLAT}" = ubuntu-14.04-amd64-64 ];then
-    FOPTMIZ=2
-elif [ "${ORDENV_PLAT}" = sles-11-amd64-64 ];then
-    FOPTMIZ=4
-elif [ "${ORDENV_PLAT}" = sles-11-broadwell-64-xc40 ];then
-    FOPTMIZ=4
-else
-    echo "... This platform 'ORDENV_PLAT=${ORDENV_PLAT}' is not supported.  Only 'ubuntu-14.04-amd64-64' and 'ubuntu-14.04-amd64-64' are."
-    exit 1
-fi
 
 # automatically set the global revision number in toplevelcontrol_mod.ftn90 by
 # replacing the string XXXXX with the actual revision number
@@ -90,81 +67,15 @@ fi
 
 absdir=${compiledir_main}/oavar_abs
 mkdir -p ${absdir}
-cd $absdir ; absdir=$PWD ; cd -
-
-#----------------------------------------------------------------
-#  Set up dependent librarys and tools. 
-#---------------------------------------------------------------
-## for s.compile
-echo "... loading hpco/tmp/eccc/201402/06/base"
-. ssmuse-sh -d hpco/tmp/eccc/201402/06/base
-## for the compiler
-if [ "${ORDENV_PLAT}" = ubuntu-14.04-amd64-64 ];then
-    echo "... loading compiler main/opt/intelcomp/intelcomp-2016.1.156"
-    . ssmuse-sh -d main/opt/intelcomp/intelcomp-2016.1.156
-elif [ "${ORDENV_PLAT}" = sles-11-amd64-64 -o "${ORDENV_PLAT}" = sles-11-broadwell-64-xc40 ];then
-    echo "... loading compiler PrgEnv-intel-5.2.82"
-    module load PrgEnv-intel/5.2.82
-else
-    echo "... This platform 'ORDENV_PLAT=${ORDENV_PLAT}' is not supported.  Only 'ubuntu-14.04-amd64-64' and 'sles-11-amd64-64' are."
-    exit 1
-fi
-
-## for netcdf
-CDF_LIBS=netcdff
+cd $absdir ; absdir=$PWD ; cd - >/dev/null
 
 varabs=oavar_${ORDENV_PLAT}${ABSTAG}-${revnum}.Abs
 
-## for rmn, rpncomm
-echo "... loading eccc/mrd/rpn/libs/16.2"
-. ssmuse-sh -d eccc/mrd/rpn/libs/16.2
-if [ "${ORDENV_PLAT}" = ubuntu-14.04-amd64-64 ];then
-    ## for openmpi
-    echo "... loading main/opt/openmpi/openmpi-1.6.5/intelcomp-2016.1.156"
-    . ssmuse-sh -d main/opt/openmpi/openmpi-1.6.5/intelcomp-2016.1.156
-elif [ "${ORDENV_PLAT}" = sles-11-amd64-64 -o "${ORDENV_PLAT}" = sles-11-broadwell-64-xc40 ];then
-    echo "loading cray-netcdf"
-    module load cray-netcdf
-fi
-## for 'vgrid'
-echo "... loading eccc/cmd/cmdn/vgrid/5.6.9/${COMP_ARCH}"
-. ssmuse-sh -d eccc/cmd/cmdn/vgrid/5.6.9/${COMP_ARCH}
-## for 'burplib'
-echo "... loading eccc/cmd/cmda/libs/16.2-4/${COMP_ARCH}"
-. ssmuse-sh -d eccc/cmd/cmda/libs/16.2-4/${COMP_ARCH}
-## For hpcoperf needed for TMG timings
-echo "... loading main/opt/perftools/perftools-2.0/${COMP_ARCH}"
-. ssmuse-sh -d main/opt/perftools/perftools-2.0/${COMP_ARCH}
-# For RTTOV 10v3 package...
-echo "... loading eccc/mrd/rpn/anl/rttov/10v3.2/${COMP_ARCH}"
-. ssmuse-sh -d eccc/mrd/rpn/anl/rttov/10v3.2/${COMP_ARCH}
-
 #-----------------------------------------------------------------------------
 
-LIBAPPL="${CDF_LIBS} rttov10.2.0_coef_io rttov10.2.0_main rttov10.2.0_emis_atlas rttov10.2.0_other burp_module descrip $MPILIB"
+LIBAPPL="netcdff rttov10.2.0_coef_io rttov10.2.0_main rttov10.2.0_emis_atlas rttov10.2.0_other burp_module descrip $MPILIB"
 LIBSYS="hpcoperf"
-
 LIBRMN=rmnMP
-
-COMPF_GLOBAL="-openmp ${MPIKEY}"
-OPTF="=-check =noarg_temp_created"
-if [ "${ORDENV_PLAT}" = ubuntu-14.04-amd64-64 ];then
-    OPTF="=-mkl ${OPTF}"
-elif [ "${ORDENV_PLAT}" = sles-11-amd64-64 -o "${ORDENV_PLAT}" = sles-11-broadwell-64-xc40 ];then
-    OPTF="${OPTF}"
-else
-    echo "... This platform 'ORDENV_PLAT=${ORDENV_PLAT}' is not supported.  Only 'ubuntu-14.04-amd64-64' and 'sles-11-amd64-64' are."
-    exit 1
-fi
-
-if [ "${COMPILE_OAVAR_ADD_DEBUG_OPTIONS:-no}" = yes ]; then
-    echo "... > !WARNING! You are compiling in DEBUG MODE"
-    COMPF_NOC="${COMPF_GLOBAL} -debug DEBUG -optf ${OPTF}"
-    COMPF="${COMPF_NOC} =-C"
-else
-    COMPF="${COMPF_GLOBAL} -optf ${OPTF}"
-    COMPF_NOC=${COMPF}
-fi
 
 if [ "${mode}" == full ] ; then
 
@@ -300,7 +211,7 @@ if [ "${mode}" == full ] ; then
   echo "... > Building the executable ${varabs}"
   rm -f ${varabs}
   echo "...   if aborting, check in ${PWD}/listing8"
-  s.compile $COMPF  -O ${FOPTMIZ} ${MPILIBDIR} -libappl $LIBAPPL $LIBEXTRA -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o ${varabs} > listing8 2>&1
+  s.compile $COMPF  -O ${FOPTMIZ} ${MPILIBDIR} -libappl $LIBAPPL -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o ${varabs} > listing8 2>&1
   status=1
   grep fail listing8 || status=0
   if [ "${status}" -ne 0 ]; then
@@ -325,7 +236,7 @@ elif [ "${mode}" == abs ] ; then
   echo "..."
   echo "... > Building the executable ${varabs}"
   echo "..."
-  s.compile $COMPF  -O ${FOPTMIZ} ${MPILIBDIR} -libappl $LIBAPPL $LIBEXTRA -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o ${varabs}
+  s.compile $COMPF  -O ${FOPTMIZ} ${MPILIBDIR} -libappl $LIBAPPL -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o ${varabs}
   cp ${varabs} ${absdir}/
 
 else
