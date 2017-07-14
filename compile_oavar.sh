fd234de32f057c34ff9e32cb8b6b948ddb17b485
Sylvain Heilliette <sylvain.heilliette@canada.ca>
2017-07-14 15:07:08 +0000

    mise a jour pour experience srttov12

--- compile_oavar.sh
+++ compile_oavar.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Revisions:
+#            Mike Sitwell, Y.J. Rochon, and Ping Du (as of Dec 2014)
+#            - The following are changes/additions for chemical constituents.
+#            - Added compilation of bmatrixchem_mod.ftn90
+#            - Addition of obssubspacedata_mod.ftn90 and chem_*_mod.ftn90 
+
 set -e
 
 mode=$1
@@ -82,6 +88,8 @@ if [ ${compiledir_main} != ".." ] ; then
     fi
 fi
 
+absdir=${compiledir_main}/abs
+mkdir -p ${absdir}
 
 #----------------------------------------------------------------
 #  Set up dependent librarys and tools. 
@@ -101,10 +109,10 @@ else
     exit 1
 fi
 
-## for netcdf
-CDF_LIBS=netcdff
+## for hdf5
+HDF5_LIBS="hdf5hl_fortran hdf5_hl hdf5_fortran hdf5 z"
 
-varabs=oavar_${ORDENV_PLAT}${ABSTAG}
+varabs=oavar_${ORDENV_PLAT}${ABSTAG}-${revnum}.Abs
 
 ## for rmn, rpncomm
 echo "... loading eccc/mrd/rpn/libs/16.1"
@@ -114,8 +122,8 @@ if [ "${ORDENV_PLAT}" = ubuntu-14.04-amd64-64 ];then
     echo "... loading main/opt/openmpi/openmpi-1.6.5/intelcomp-2016.1.156"
     . ssmuse-sh -d main/opt/openmpi/openmpi-1.6.5/intelcomp-2016.1.156
 elif [ "${ORDENV_PLAT}" = sles-11-amd64-64 -o "${ORDENV_PLAT}" = sles-11-broadwell-64-xc40 ];then
-    echo "loading cray-netcdf"
-    module load cray-netcdf
+    echo "loading cray-hdf5"
+    module load cray-hdf5
 fi
 ## for 'vgrid'
 echo "... loading eccc/cmd/cmdn/vgrid/5.6.9/${COMP_ARCH}"
@@ -126,13 +134,9 @@ echo "... loading eccc/cmd/cmda/libs/16.1-3/${COMP_ARCH}"
 ## For hpcoperf needed for TMG timings
 echo "... loading main/opt/perftools/perftools-2.0/${COMP_ARCH}"
 . ssmuse-sh -d main/opt/perftools/perftools-2.0/${COMP_ARCH}
-# For RTTOV 10v3 package...
-echo "... loading eccc/mrd/rpn/anl/rttov/10v3.2/${COMP_ARCH}"
-. ssmuse-sh -d eccc/mrd/rpn/anl/rttov/10v3.2/${COMP_ARCH}
-
 #-----------------------------------------------------------------------------
 
-LIBAPPL="${CDF_LIBS} rttov10.2.0_coef_io rttov10.2.0_main rttov10.2.0_emis_atlas rttov10.2.0_other burp_module descrip $MPILIB"
+LIBAPPL="rttov_coef_io rttov_hdf rttov_parallel  rttov_main rttov_emis_atlas rttov_other ${HDF5_LIBS} burp_module descrip $MPILIB"
 LIBSYS="hpcoperf"
 
 LIBRMN=rmnMP
@@ -155,6 +159,15 @@ if [ "${COMPILE_OAVAR_ADD_DEBUG_OPTIONS:-no}" = yes ]; then
 else
     COMPF="${COMPF_GLOBAL} -optf ${OPTF}"
     COMPF_NOC=${COMPF}
+fi
+
+echo "...   loading arma/rttov/12v1"
+export LD_LIBRARY_PATH="/home/syh074/data_maestro/eccc-ppp2/rttov12/lib:${LD_LIBRARY_PATH}"
+export MY_INCLUDE_PATH="/home/syh074/data_maestro/eccc-ppp2/rttov12/includes ${MY_INCLUDE_PATH}"
+if [ "${ORDENV_PLAT}" = ubuntu-14.04-amd64-64 ];then
+    echo "...   loading hdf5"
+    export LD_LIBRARY_PATH="/home/syh074/data_maestro/eccc-ppp2/hdf5-1.8.17/hdf5/lib:${LD_LIBRARY_PATH}"
+    export MY_INCLUDE_PATH="/home/syh074/data_maestro/eccc-ppp2/hdf5-1.8.17/hdf5/include ${MY_INCLUDE_PATH}"
 fi
 
 if [ "${mode}" == full ] ; then
@@ -223,11 +236,11 @@ if [ "${mode}" == full ] ; then
   SRC1="obssubspacedata_mod.ftn90 controlvector_mod.ftn90 rmatrix_mod.ftn90 hirchannels_mod.ftn90 "
   SRC1="$SRC1 varnamelist_mod.ftn90 columndata_mod.ftn90 ozoneclim_mod.ftn90 tovs_nl_mod.ftn90 multi_ir_bgck_mod.ftn90"
   SRC1="$SRC1 globalspectraltransform_mod.ftn90 tt2phi_mod.ftn90"
-  SRC1="$SRC1 lamspectraltransform_mod.ftn90 gridstatevector_mod.ftn90 statetocolumn_mod.ftn90 variabletransforms_mod.ftn90"
+  SRC1="$SRC1 lamspectraltransform_mod.ftn90 gridstatevector_mod.ftn90 ensemblestatevector_mod.ftn90 statetocolumn_mod.ftn90 variabletransforms_mod.ftn90"
   SRC1="$SRC1 bmatrixchem_mod.ftn90 bmatrixensemble_mod.ftn90 bmatrixhi_mod.ftn90 lambmatrixhi_mod.ftn90"
   SRC1="$SRC1 bmatrix_mod.ftn90 residual_mod.ftn90 costfunction_mod.ftn90"
 
-  s.compile $COMPF  -O ${FOPTMIZ} -src $SRC1 > listing3 2>&1
+  s.compile -includes $MY_INCLUDE_PATH $COMPF  -O ${FOPTMIZ} -src $SRC1  > listing3 2>&1
   status=1
   grep fail listing3 || status=0
   if [ "${status}" -ne 0 ]; then
@@ -261,7 +274,7 @@ if [ "${mode}" == full ] ; then
   echo "...   if aborting, check in ${PWD}/listing6"
   SRC2="burpfiles_mod.ftn90 chem_obserrors_mod.ftn90 obserrors_mod.ftn90 varqc_mod.ftn90 chem_setup_mod.ftn90 obsfilter_mod.ftn90 tovs_lin_mod.ftn90 chem_obsoperators_mod.ftn90 obsoperators_mod.ftn90 obsspacediag_mod.ftn90  innovation_mod.ftn90"
   SRC2="$SRC2 chem_postproc_mod.ftn90 minimization_mod.ftn90"
-  s.compile $COMPF  -O ${FOPTMIZ} -src $SRC2 > listing6 2>&1
+  s.compile -includes $MY_INCLUDE_PATH $COMPF  -O ${FOPTMIZ} -src $SRC2 > listing6 2>&1
   status=1
   grep fail listing6 || status=0
   if [ "${status}" -ne 0 ]; then
@@ -277,7 +290,7 @@ if [ "${mode}" == full ] ; then
 	  filelist="$filelist ${i}"
       fi
   done
-  s.compile $COMPF  -O ${FOPTMIZ} -src $filelist > listing7 2>&1
+  s.compile -includes $MY_INCLUDE_PATH $COMPF  -O ${FOPTMIZ} -src $filelist  > listing7 2>&1
   status=1
   grep fail listing7 || status=0
   if [ "${status}" -ne 0 ]; then
@@ -285,10 +298,10 @@ if [ "${mode}" == full ] ; then
       exit 1
   fi
 
-  echo "... > Building the executable ${varabs}.Abs"
-  rm -f ${varabs}.Abs
+  echo "... > Building the executable ${varabs}"
+  rm -f ${varabs}
   echo "...   if aborting, check in ${PWD}/listing8"
-  s.compile $COMPF  -O ${FOPTMIZ} ${MPILIBDIR} -libappl $LIBAPPL $LIBEXTRA -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o ${varabs}.Abs > listing8 2>&1
+  s.compile $COMPF  -O ${FOPTMIZ} ${MPILIBDIR} -libappl $LIBAPPL $LIBEXTRA -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o ${varabs} > listing8 2>&1
   status=1
   grep fail listing8 || status=0
   if [ "${status}" -ne 0 ]; then
@@ -302,18 +315,19 @@ if [ "${mode}" == full ] ; then
       echo "... !! ERROR found: STOP; check listing in ${PWD} !!"
       exit 1
   fi
+  cp ${varabs} ${absdir}/
 
   #### rm -f *.ftn* *.f *.f90
 
 elif [ "${mode}" == abs ] ; then
 
-  rm -f ${varabs}.Abs
+  rm -f ${varabs}
 
   echo "..."
-  echo "... building the executable ${varabs}.Abs"
+  echo "... > Building the executable ${varabs}"
   echo "..."
-  set -x
-  s.compile $COMPF  -O ${FOPTMIZ} -libappl $LIBAPPL $LIBEXTRA -librmn $LIBRMN -obj *.o -o ${varabs}.Abs
+  s.compile $COMPF  -O ${FOPTMIZ} ${MPILIBDIR} -libappl $LIBAPPL $LIBEXTRA -libsys $LIBSYS -librmn $LIBRMN -obj *.o -o ${varabs}
+  cp ${varabs} ${absdir}/
 
 else
     if [ -f $trunkdir/$mode ] ; then
@@ -323,7 +337,7 @@ else
 	echo "..."
 	echo "... compiling $mode"
 	echo "..."
-	s.compile $COMPF  -O ${FOPTMIZ} -src $file
+	s.compile -includes $MY_INCLUDE_PATH $COMPF  -O ${FOPTMIZ} -src $file
     else
 	echo "..."
 	echo "... !! File $trunkdir/$mode does NOT exist. Stop !!"
@@ -335,7 +349,7 @@ echo "..."
 echo "... > FINISHED COMPILATION AT: $(date)"
 if [ "${mode}" == full -o "${mode}" == abs ] ; then
     echo "..."
-    echo "... The program can be found here: ${PWD}/${varabs}.Abs"
+    echo "... The program can be found here: ${PWD}/${varabs}"
     echo "..."
 else
     echo "..."
