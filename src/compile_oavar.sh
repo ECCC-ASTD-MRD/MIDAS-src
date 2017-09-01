1f5ab8794e4a8d6453a311885a74a2afdbc14ed6
Ping Du <ping.du@canada.ca>
2017-09-01 14:39:44 +0000

    Include fso_mod in compile_oavar

--- src/compile_oavar.sh
+++ src/compile_oavar.sh
@@ -188,6 +188,7 @@ if [ "${mode}" == full ] ; then
   SRC2="obssubspacedata_mod.ftn90 burpfiles_mod.ftn90 chem_setup_mod.f90 chem_obserrors_mod.f90"
   SRC2="$SRC2 chem_obsoperators_mod.f90 chem_postproc_mod.f90"
   SRC2="$SRC2 obserrors_mod.f90 varqc_mod.f90 obsfilter_mod.f90 obsoperators_mod.f90 obsspacediag_mod.f90"
+  SRC2="$SRC2 fso_mod.f90"
   SRC2="$SRC2 innovation_mod.f90 minimization_mod.f90"
   s.compile $COMPF  -O ${FOPTMIZ} -src $SRC2 > listing6 2>&1
   status=1
