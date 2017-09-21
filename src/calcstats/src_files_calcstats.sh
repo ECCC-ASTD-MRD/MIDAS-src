007a2e2e314c4f4f96cdcbbf0c4fbff29f957afb
Joel Bedard <joel.bedard@canada.ca>
2017-09-21 15:30:00 -0400

    tout compile, roule et valide maintenant... sauf calcstats

---
+++ src/calcstats/src_files_calcstats.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+LIBAPPL="descrip $MPILIB"
+
+SRC_FILES="randomnumber_mod.f90 utilities_mod.f90 toplevelcontrol_mod.f90 bufr_mod.f90"
+SRC_FILES="$SRC_FILES mathphysconstants_mod.f90 earthconstants_mod.f90 mpi_mod.f90 mpivar_mod.f90"
+SRC_FILES="$SRC_FILES physicsfunctions_mod.f90 horizontalcoord_mod.f90 obsspacedata_mod.f90"
+SRC_FILES="$SRC_FILES controlvector_mod.f90 varnamelist_mod.f90 timecoord_mod.f90 localizationfunction_mod.f90"
+SRC_FILES="$SRC_FILES globalspectraltransform_mod.f90 verticalcoord_mod.f90"
+SRC_FILES="$SRC_FILES lamspectraltransform_mod.f90 gridstatevector_mod.f90 spectralfilter_mod.f90"
+SRC_FILES="$SRC_FILES columndata_mod.f90 analysisgrid_mod.f90 lambmatrixhi_mod.f90"
+SRC_FILES="$SRC_FILES calcstatsglb_mod.f90 calcstatslam_mod.f90"
