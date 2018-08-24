57f0223f65cddac42c8f2a836fa73652e157fafd
Jeff Blezius <jeffrey.blezius@canada.ca>
2018-08-24 14:04:52 -0400

    Iss #110 Create a system test for the thinning module
    - There being no data at the moment, the test fails.
    - The main program being tested, midas_thinning, was copied from program var, but
      most of the program content has been removed.
    - Because the thinning module has not yet been introduced, program midas_thinning
      doesn't even do any thinning.

---
+++ maestro/suites/midas_system_tests/modules/UnitTest/scripts/thinning.sh
@@ -0,0 +1,152 @@
+#!/bin/ksh
+
+set -ex
+
+SECONDS=0
+
+fasttmp=${1}
+ramdiskpath=${2:-/tmp/${USER}}
+
+## the observations are split into many files
+splitobs=yes
+
+npex=${SEQ_NPEX}
+npey=${SEQ_NPEY}
+let nprocs=npex*npey
+
+typeset -Z4 obsfile_x obsfile_y
+
+obsfile_y=$((npey-MP_CHILD/npex))
+obsfile_x=$((MP_CHILD+1-npex*(MP_CHILD/npex)))
+
+if [ "${fasttmp}" = yes ]; then
+    FASTTMPDIR=${ramdiskpath}/oavar_${MP_CHILD}
+    /bin/mkdir -p ${FASTTMPDIR}
+
+    ## move observations in RAMDisk
+    /bin/mkdir ${FASTTMPDIR}/obs
+    if [ "${splitobs}" = no ]; then
+        /bin/cp brp* ${FASTTMPDIR}/obs
+    else
+        for file in burpfiles_*/brp*_${obsfile_x}_${obsfile_y} \
+                    sqlfiles_*/obs*_${obsfile_x}_${obsfile_y}; do
+            [ -f "${file}" ] && /bin/cp ${file} ${FASTTMPDIR}/obs
+        done
+    fi
+    export OAVAR_BURP_DIRECTORY=${FASTTMPDIR}/obs
+
+    ## Move Ensemble files in RAMDisk
+    if [ -d ensemble ]; then
+        /bin/mkdir ${FASTTMPDIR}/ensemble
+        for file in ensemble/2*; do
+        ## the file ensemble/2018020500_006_0128
+        ##    -> 2018020500_006_0128
+        ##      -> 8210_600_0050208102
+        ##        -> 8210
+        ##          -> 0128 (which is 'number')
+            number=$(/usr/bin/basename ${file} | /usr/bin/rev | /usr/bin/cut -d_ -f1 | /usr/bin/rev)
+            if [ "$(((number-1)%nprocs))" -eq "${MP_CHILD}" ]; then
+                /bin/cp ensemble/*_${number} ${FASTTMPDIR}/ensemble
+            fi
+        done
+        export OAVAR_ENSPATHNAME=${FASTTMPDIR}/ensemble
+    fi
+
+    ## Move Trial files in RAMDisk
+    typeset -Z2 counter=1
+    for file in trlm_* ; do
+        if [ "$(((counter-1)%nprocs))" -eq "${MP_CHILD}" ]; then
+            /bin/cp ${file} ${FASTTMPDIR}/
+        fi
+	let counter=counter+1
+    done
+
+    /bin/df -hP ${FASTTMPDIR}
+
+    export OAVAR_RAM_DISK_DIR=${FASTTMPDIR}
+else
+    if [ "${splitobs}" != no ]; then
+       [ "${MP_CHILD}" -eq 0 ] && /bin/mkdir ./obs
+       while [ ! -d ./obs ]; do
+           /bin/sleep 1
+       done
+       if [ "$(echo burpfiles_*/brp*)" != "burpfiles_*/brp*" ]; then
+          /bin/cp burpfiles_*/brp*_${obsfile_x}_${obsfile_y} ./obs
+          export OAVAR_BURP_DIRECTORY=./obs
+       fi
+       if [ "$(echo sqlfiles_*/obs*)" != "sqlfiles_*/obs*" ]; then
+          /bin/cp sqlfiles_*/obs*_${obsfile_x}_${obsfile_y} ./obs
+       fi
+    fi
+fi
+
+echo "The preparation of the working directory took ${SECONDS} seconds"
+
+SECONDS=0
+${RUN_PGM}
+echo "The program itself took ${SECONDS} seconds"
+
+SECONDS=0
+
+if [ "${splitobs}" = no ]; then
+    if [ "${fasttmp}" = yes ]; then
+        if [ "${MP_CHILD}" -eq 0 ]; then
+            /bin/cp ${FASTTMPDIR}/obs/brp* .
+        fi
+    fi
+else
+    if [ "${fasttmp}" = yes ]; then
+        for file in ${FASTTMPDIR}/obs/brp*; do
+            if [ -f "${file}" ]; then
+                fam=$(/usr/bin/basename ${file} | /usr/bin/rev | /usr/bin/cut -d_ -f3- | /usr/bin/rev | /usr/bin/cut -c4-)
+                while [ ! -d burpfiles_${fam}.thinned ]; do
+                    if [ "${MP_CHILD}" -eq 0 ]; then
+                        /bin/mkdir burpfiles_${fam}.thinned
+                        break
+                    fi
+                    sleep 1
+                done
+                /bin/cp ${file} burpfiles_${fam}.thinned/$(/usr/bin/basename ${file})
+            fi
+        done
+    else
+        if [ "$(echo burpfiles_*/brp*)" != "burpfiles_*/brp*" ]; then
+            for file in ./obs/brp*_${obsfile_x}_${obsfile_y}; do
+                fam=$(/usr/bin/basename ${file} | /usr/bin/rev | /usr/bin/cut -d_ -f3- | /usr/bin/rev | /usr/bin/cut -c4-)
+                while [ ! -d burpfiles_${fam}.thinned ]; do
+                    if [ "${MP_CHILD}" -eq 0 ]; then
+                        /bin/mkdir burpfiles_${fam}.thinned
+                        break
+                    fi
+                    sleep 1
+                done
+                /bin/mv ${file} burpfiles_${fam}.thinned/$(/usr/bin/basename ${file})
+            done
+        fi
+
+        if [ "$(echo sqlfiles_*/obs*)" != "sqlfiles_*/obs*" ]; then
+            for file in ./obs/obs*_${obsfile_x}_${obsfile_y}; do
+                fam=$(/usr/bin/basename ${file} | /usr/bin/rev | /usr/bin/cut -d_ -f3- | /usr/bin/rev | /usr/bin/cut -c4-)
+                while [ ! -d sqlfiles_${fam}.thinned ]; do
+                    if [ "${MP_CHILD}" -eq 0 ]; then
+                        /bin/mkdir sqlfiles_${fam}.thinned
+                        break
+                    fi
+                    sleep 1
+                done
+
+                # Clean deleted records from the SQL file
+                sqlite3 ${file} 'vacuum'
+
+                /bin/mv ${file} sqlfiles_${fam}.thinned/$(/usr/bin/basename ${file})
+            done
+        fi
+    fi
+fi
+
+if [ "${fasttmp}" = yes ]; then
+    /bin/df -hP ${FASTTMPDIR}
+    /bin/rm -rf ${FASTTMPDIR}
+fi
+
+echo "The finalization took ${SECONDS} seconds"
---
+++ src/programs/thinning.f90
@@ -0,0 +1,122 @@
+!-------------------------------------- LICENCE BEGIN ------------------------------------
+!Environment Canada - Atmospheric Science and Technology License/Disclaimer,
+!                     version 3; Last Modified: May 7, 2008.
+!This is free but copyrighted software; you can use/redistribute/modify it under the terms
+!of the Environment Canada - Atmospheric Science and Technology License/Disclaimer
+!version 3 or (at your option) any later version that should be found at:
+!http://collaboration.cmc.ec.gc.ca/science/rpn.comm/license.html
+!
+!This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+!without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+!See the above mentioned License/Disclaimer for more details.
+!You should have received a copy of the License/Disclaimer along with this software;
+!if not, you can write to: EC-RPN COMM Group, 2121 TransCanada, suite 500, Dorval (Quebec),
+!CANADA, H9P 1J3; or send e-mail to service.rpn@ec.gc.ca
+!-------------------------------------- LICENCE END --------------------------------------
+
+!--------------------------------------------------------------------------
+!!
+!! *Purpose*: Thinning1 program to reduce the number of observation data
+!!
+!--------------------------------------------------------------------------
+program midas_thinning
+  use ramDisk_mod
+  use utilities_mod
+  use mpivar_mod
+  use obsSpaceData_mod
+  use obsFiles_mod
+  use innovation_mod
+
+  implicit none
+
+  integer :: istamp,exdb,exfin
+  integer :: ierr
+  type(struct_obs)               :: obsSpaceData
+  character(len=48) :: obsMpiStrategy, varMode
+
+  istamp = exdb('THINNING','DEBUT','NON')
+
+  write(*,'(/,' //                                                &
+            '3(" *****************"),/,' //                       &
+            '14x,"-- START OF MAIN PROGRAM MIDAS-THINNING: --",/,' //   &
+            '14x,"-- OBSERVATION THINNING          --",/, ' //&
+            '14x,"-- VAR Revision number   ",a," --",/,' //       &
+            '3(" *****************"))') 'GIT-REVISION-NUMBER-WILL-BE-ADDED-HERE'
+
+  ! MPI initilization
+  call mpi_initialize  
+
+  call tmg_init(mpi_myid, 'TMG_THIN' )
+  call tmg_start(1,'MAIN')
+
+  varMode='bgckConv'  ! a necessary argument for obsf_setup
+
+
+  ! 1. Top level setup
+
+  call ram_setup
+
+
+  ! 2. configuration the job
+
+  ! Do initial set up
+  call tmg_start(2,'PREMIN')
+
+  obsMpiStrategy = 'LIKESPLITFILES'
+
+  call thin_setup('ALL') ! obsColumnMode 
+
+
+!!$  ! 3. Do the Thinning - set bit9 of iflag
+!!$  call obsf_thinFiles(obsSpaceData)
+
+  ! 4. Job termination
+
+  istamp = exfin('THINNING','FIN','NON')
+
+!!$  ! deallocate obsSpaceData
+!!$  call obs_finalize(obsSpaceData)
+
+
+  call tmg_stop(1)
+  call tmg_terminate(mpi_myid, 'TMG_THIN' )
+
+  call rpn_comm_finalize(ierr)
+
+contains
+
+  !--------------------------------------------------------------------------
+  !! *Purpose*: Control of the preprocessing of the variational assimilation
+  !!
+  !! Revisions:
+  !!           Y.J. Rochon, Jan 2016
+  !!           - Addition of test on availability of input trial fields according
+  !!             to related observation families.
+  !--------------------------------------------------------------------------
+  subroutine thin_setup(obsColumnMode)
+    implicit none
+
+    character (len=*) :: obsColumnMode
+    integer :: datestamp
+
+    integer :: get_max_rss
+
+    write(*,*) ''
+    write(*,*) '-----------------------------------'
+    write(*,*) '-- Starting subroutine thin_setup --'
+    write(*,*) '-----------------------------------'
+
+!!$    !     
+!!$    !- Initialize observation file names
+!!$    !
+!!$    call obsf_setup( dateStamp, varMode )
+
+    !
+    !- Setup and read observations
+    !
+!!$    call inn_setupObs(obsSpaceData, obsColumnMode, obsMpiStrategy, varMode) ! IN
+!!$    write(*,*) 'Memory Used: ',get_max_rss()/1024,'Mb'
+
+  end subroutine thin_setup
+
+end program midas_thinning
