7280109c5d201b41b13f4803f7b1c48e4fd4f436
Jeff Blezius <jeffrey.blezius@canada.ca>
2018-08-24 14:16:00 -0400

    Iss #110 Delete commented development notes
    - Also adjust src_files_thinning.sh to compensate for recent rebase

--- src/modules/thinning_mod.f90
+++ src/modules/thinning_mod.f90
@@ -1,4 +1,3 @@
-
 !--------------------------------------- LICENCE BEGIN -----------------------------------
 !Environment Canada - Atmospheric Science and Technology License/Disclaimer,
 !                     version 3; Last Modified: May 7, 2008.
@@ -22,36 +21,14 @@
 !!
 !--------------------------------------------------------------------------
 module thinning_mod
-!!$  use timeCoord_mod
-!!$  use obsFiles_mod
   use bufr_mod
   use obsSpaceData_mod
-    use utilities_mod
-    use fSQLite
+  use utilities_mod
   implicit none
   private
   public thn_thinAladin
 
 contains
-
-!!$!tim_getStepObsIndex() ! replaces stepobs(), 
-!!$                       ! but last arg is now numStep rather than dddt
-!!$
-!!$get_analysis_time() ! in BGCKALT record, used by iasi, scat, satwinds
-!!$
-!!$element_list = (/ values according to family type /)
-!!$
-!!$IdentifyObsBoxForAllObservations()
-!!$  {! Loop over observations once and do this for each obs:
-!!$      identify_time_bin_for_the_obs() --> call tim_getStepObsIndex()
-!!$      identify_layer_for_the_obs()
-!!$      identify_latlon_bin_for_the_obs()
-!!$      separa() de Peter H. - for calculating deltrad (iasi)
-!!$      [also calculate the time, layer, distance differences?]
-!!$   return
-!!$  }
-!!$
-!!$set_spatial_grid_boxes()  ! used by iasi, scat
 
   subroutine thn_thinAladin(obsdat)
     implicit none
@@ -60,29 +37,15 @@ contains
     type(struct_obs), intent(inout) :: obsdat
 
 
-    ! deltrad:  iasi also uses this in addition to thinningSteps,
-    !           thinningDistance.  The radial distance from box centre.
-    ! scat:  uses deltax1 and deltax2, the latter being for the resolution of
-    !        circumpolar orbits (???)
-
-
     ! NAMELIST VARIABLES
-    integer :: nulnam, ii
-    real    :: stepHours ! assimilation step size
     integer :: keepNthVertical ! keep every nth vertical datum
 
-    ! Distance that limits to one observation per box.
-    ! Depending on the strategy, this distance is either between two observations
-    ! or between an observation and the thinning-box centre.
-    real :: thinningDistance
-
-    namelist /thin_aladin/stepHours, thinningDistance, keepNthVertical
+    namelist /thin_aladin/keepNthVertical
 
 
     ! LOCAL VARIABLES
-    integer :: fnom, fclos, ierr, flag
-!!$    integer :: numStep ! number of assimilation steps in the assimilation window
-!!$    integer :: obsDateStamp
+    integer :: nulnam
+    integer :: fnom, fclos, ierr
 
 
 
@@ -95,81 +58,8 @@ contains
     write(*,nml=thin_aladin)
     ierr=fclos(nulnam)
 
-    ! For now, Aladin needs no thinning.  Do nothing.
-    ! For now, Aladin needs no thinning.  Do nothing.
-    ! For now, Aladin needs no thinning.  Do nothing.
-
     call keepNthObs(obsdat, 'AL', keepNthVertical)
 
-
-!!$    ! Calculate the number of time bins in the assimilation window
-!!$    numStep = 2*nint((3.d0 - stepHours/2.d0)/stepHours) + 1
-!!$    ! Calculate the number of horizontal boxes (for iasi, at least)
-!!$    ! Calculate the number of vertical boxes (for iasi, at least)
-!!$
-!!$    obsf_cfilnam(1)='obsal_0001_0001'
-!!$    call obsf_setup(obsDateStamp, 'thinning')
-!!$    call obsf_readFiles(obsSpaceData)
-!!$
-!!$    Create outFileName
-!!$
-!!$    Scan le fichier d'entree pour chercher:
-!!$      - la date et heure de l'enregistrement resume (date, temps)
-!!$      - le nombre de rapports (nb_rpts)
-!!$      - le nombre maximum d''observations par rapport (nb_obs_max)
-!!$
-!!$    ! ALGORITHM DISCREPANCY, depending on observation type:
-!!$    ! =====================================================
-!!$    Allocate arrays based on nb_rpts, nb_obs_max ! i.e. obs reports
-!!$    (otherwise de- and re-allocate on each pass through the loop)
-!!$    ! NOTE:  scat allocates based on horizontal, vertical, temporal boxes;
-!!$    !        i.e. observation cubes.  But it does not attempt to save all
-!!$    !        observation attributes for each box.
-!!$    ! whereas satwinds, radiosondes, iasi allocate based on observation reports
-!!$    ! (but iasi re-allocates for every record, not for all obs in the file)
-!!$    !         --> during selection, SW loops over the observations and rejects
-!!$    !             the obs if it is within a radius of distance_thin of another
-!!$    !             selected obs
-!!$    !         --> during selection, radiosondes loops over the observations
-!!$    !             applying selection criteria, keeping only the best in a box
-!!$    !         --> during selection, iasi loops over observations and keeps all
-!!$    !             obs that satisfy its criteria
-!!$    !         --> for each BURP report, scat loops over the observations and
-!!$    !             calls scat_select().  Scat_select identifies the observation
-!!$    !             cube that fits the obs and checks to see whether the
-!!$    !             observation is best so far for that cube
-!!$    !
-!!$    ! Because there are different approaches, I cannot do the allocation
-!!$    ! automatically in a service of this module.
-!!$
-!!$    ierr = newdate(middleDateStamp, date, temps*10000, 3)
-!!$    call tim_getstamplist(dateStampList, numStep, middleDateStamp)
-!!$
-!!$    do records in inFileName
-!!$      Ignore some records, based on idtyp
-!!$      read pertinent values (different for each obs type)
-!!$      adjust longitude value
-!!$      call identify_time_bin_for_the_obs()
-!!$      call identify_layer_for_the_obs()
-!!$      record all of these values in arrays
-!!$    end do
-!!$
-!!$    call thinIt_aladin()
-!!$    ! satwinds:  calls it 'BRUTE'
-!!$    ! radiosondes:  calls it simply 'thinning_model', 'thinning_es', 
-!!$    !                               'backlisting_ecmwf'
-!!$    ! iasi:  called simply 'thinning' in one monolithic routine
-!!$    !        (thinning rules given in comments at beginning of file)
-!!$    ! scat:  called 'scat_select'
-!!$
-!!$    Calculate the number of observations per report for the output file
-!!$
-!!$    do selected reports
-!!$      Transfer data (from inFileName [or from memory?] to outFileName
-!!$                    (different data for each obs type)
-!!$    end do
-!!$
-!!$    deallocate
   end subroutine thn_thinAladin
 
 
