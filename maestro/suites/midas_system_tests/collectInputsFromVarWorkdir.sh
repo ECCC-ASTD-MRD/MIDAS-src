d11f210bf14799634b3cffd636bcbfb10ea1de28
Ervig Lapalme <ervig.lapalme@canada.ca>
2018-10-04 19:53:51 +0000

    collectInputsFromVarWorkdir: It is now working with newer versions of MIDAS.

--- maestro/suites/midas_system_tests/collectInputsFromVarWorkdir.sh
+++ maestro/suites/midas_system_tests/collectInputsFromVarWorkdir.sh
@@ -26,7 +26,11 @@ fi
 mkdir -v ${destination}
 
 for file in ${source}/*; do
-    if [[ "$(basename ${file})" = burpfiles_*.updated ]]; then
+    if [[ "$(basename ${file})" = burpfiles* ]]; then
+	continue
+    elif [[ "$(basename ${file})" = sqlfiles* ]]; then
+	continue
+    elif [[ "$(basename ${file})" = obsfiles* ]]; then
 	continue
     elif [[ "$(basename ${file})" = *splitobs* ]]; then
 	continue
@@ -46,15 +50,96 @@ for file in ${source}/*; do
 	continue
     elif [[ "$(basename ${file})" = rebm* ]]; then
 	continue
+    elif [[ "$(basename ${file})" = rehm* ]]; then
+	continue
+    elif [[ "$(basename ${file})" = anlm* ]]; then
+	continue
+    elif [[ "$(basename ${file})" = *.Abs ]]; then
+	continue
+    elif [[ "$(basename ${file})" = *hostname* ]]; then
+	continue
     elif [[ "$(basename ${file})" = core* ]]; then
 	continue
+    elif [[ "$(basename ${file})" = ensemble* ]]; then
+	continue
+    elif [[ "$(basename ${file})" = *nml ]]; then
+	continue
+    elif [[ "$(basename ${file})" = *barrier* ]]; then
+	continue
+    elif [[ "$(basename ${file})" =  corvert_modular.fst ]]; then
+	continue
+    elif [[ "$(basename ${file})" =  VAR3D_STATUS.dot ]]; then
+	continue
     fi
-    echo "Copying '${file}' to '${destination}'"
-    ${do_copy_file} cp -rL ${file} ${destination}
+    echo "Linking '${file}' to '${destination}'"
+    ${do_copy_file} ln -s ${file} ${destination}
 done
 
 cd ${destination}
-cmcarc -f .inputs.ca -v --md5 -a *
-rm -rf *
+cmcarc -f .inputs.ca -v --md5 --dereference -a *
+chmod -v -wx .inputs.ca
+rm *
+
+for file in ${source}/{obs,sql,burp}files*; do
+    if [[ "$(basename ${file})" = *files_*.updated ]]; then
+	continue
+    fi
+    echo "Linking '${file}' to '${destination}'"
+    if [ -d "${file}" ]; then
+        ${do_copy_file} ln -s ${file} ${destination}
+        for thisfile in $(basename ${file})/*; do
+            [ -f "${thisfile}" ] && cmcarc -f .inputs_obsfiles.ca -v --md5 --dereference -a ${thisfile}
+        done
+        rm $(basename ${file})
+    fi
+done
+chmod -v -wx .inputs_obsfiles.ca
+
+for file in ${source}/{obs,sql,burp}files*.updated; do
+    if [ -d "${file}" ]; then
+        echo "Linking '${file}' to '${destination}'"
+        ${do_copy_file} ln -s ${file} ${destination}
+        for thisfile in $(basename ${file})/*; do
+            [ -f "${thisfile}" ] && cmcarc -f .results_obsfiles.ca -v --md5 --dereference -a ${thisfile}
+        done
+        rm $(basename ${file})
+    fi
+done
+if [ -f .results_obsfiles.ca ]; then
+    chmod -v -wx .results_obsfiles.ca
+fi
+
+for bah in rebm rehm anlm; do
+    copied_something=no
+    for file in ${source}/${bah}* ${source}/../${bah}*; do
+        echo "Linking '${file}' to '${destination}'"
+        if [ -f "${file}" ]; then
+            ${do_copy_file} ln -s ${file} ${destination}
+            copied_something=yes
+        fi
+    done
+    if [ "${copied_something}" = yes ]; then
+        cmcarc -f .results_${bah}.ca -v --md5 --dereference -a ${bah}*
+        chmod -v -wx .results_${bah}.ca
+        rm *
+    fi
+done
+
 mv -v .inputs.ca inputs.ca
-chmod -v -wx inputs.ca
+mv -v .inputs_obsfiles.ca inputs_obsfiles.ca
+if [ -f .results_obsfiles.ca ]; then
+    mv -v .results_obsfiles.ca results_obsfiles.ca
+fi
+for bah in rebm rehm anlm; do
+    if [ -f .results_${bah}.ca ]; then
+        mv -v .results_${bah}.ca results_${bah}.ca
+    fi
+done
+
+if [ -d ${source}/ensemble ]; then
+    echo "Linking 'ensemble' to '${destination}'"
+    ${do_copy_file} ln -s ${source}/ensemble ensemble
+    cmcarc -f inputs_ensemble.ca -v --md5 --dereference -a ensemble/*
+    chmod -v -wx inputs_ensemble.ca
+    rm ensemble
+fi
