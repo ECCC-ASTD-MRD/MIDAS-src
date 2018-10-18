acac3db5f5c5c0f47ef8335e35e1a60051acf1ef
Jeff Blezius <jeffrey.blezius@canada.ca>
2018-10-18 13:01:53 -0400

    Iss #114 Read the profile ID from SQL files into struct_obs (obsSpaceData)
    - Add OBS_PRFL to obsSpaceData to receive the profile ID
    - Arrange id_prof to be read from the sql header for aladin observations

--- src/modules/sqlite_read_mod.f90
+++ src/modules/sqlite_read_mod.f90
@@ -61,14 +61,16 @@ contains
 
   subroutine sqlr_initHeader( obsdat, rdbSchema, familyType, headerIndex, elev, obsSat, azimuth, geoidUndulation, &
                               earthLocRadCurv, roQcFlag, instrument, zenith, cloudCover, solarZenith, &
-                              solarAzimuth, landSea, obsIdo, obsLat, obsLon, codeType, obsDate, obsTime, obsStatus, idStation )
+                              solarAzimuth, landSea, obsIdo, obsLat, obsLon, codeType, obsDate, obsTime, &
+                              obsStatus, idStation, id_prof)
     implicit none
     ! arguments
     type (struct_obs), intent(inout) :: obsdat
     character(len=*) , intent(in)    :: rdbSchema,idStation
     character(len=2) , intent(in)    :: familyType
     integer          , intent(in)    :: headerIndex, obsIdo, codeType, obsDate, obsTime, obsStatus, azimuth, landSea, roQcFlag
-    integer          , intent(in)    :: obsSat, instrument, zenith, cloudCover, solarZenith, solarAzimuth
+    integer          , intent(in)    :: obsSat, instrument, zenith, cloudCover, solarZenith, &
+                                        solarAzimuth, id_prof
     real(obs_real)   , intent(in)    :: geoidUndulation, earthLocRadCurv, elev, obsLat, obsLon
 
     call obs_setFamily( obsdat, trim(familyType), headerIndex       )
@@ -110,6 +112,8 @@ contains
         call obs_headSet_r( obsdat, OBS_TRAD, headerIndex, earthLocRadCurv )
         call obs_headSet_i( obsdat, OBS_SAT , headerIndex, obsSat          )
         call obs_headSet_i( obsdat, OBS_AZA , headerIndex, azimuth         )
+      else if ( trim(rdbSchema) == 'al' ) then
+        call obs_headSet_i( obsdat, OBS_PRFL, headerIndex, id_prof         )
       end if
 
     end if
@@ -129,7 +133,7 @@ contains
     integer                  :: obsIdo, obsIdd, codeType, obsDate, obsTime, obsStatus, obsFlag, obsVarno
     real(obs_real)           :: elevReal, xlat, xlon, vertCoord
     real                     :: elev    , obsLat, obsLon, elevFact
-    integer                  :: azimuth, vertCoordType, vertCoordFact, fnom, fclos, nulnam, ierr 
+    integer                  :: azimuth, vertCoordType, vertCoordFact, fnom, fclos, nulnam, ierr, id_prof
     real                     :: zenithReal, solarZenithReal, CloudCoverReal, solarAzimuthReal
     integer                  :: zenith    , solarZenith    , CloudCover    , solarAzimuth    , roQcFlag
     real(obs_real)           :: geoidUndulation, earthLocRadCurv, azimuthReal, obsValue, surfEmiss
@@ -240,7 +244,8 @@ contains
         read(nulnam, nml = NAMSQLpr, iostat = ierr )
         if (ierr /= 0 ) call utl_abort( myError//': Error reading namelist' )
         if (mpi_myid == 0) write(*, nml =  NAMSQLpr )
-      case ( 'al' )
+      case ( 'al' )  
+        columnsHeader = trim(columnsHeader)//", id_prof"
         vertCoordFact = 1
         vertCoordType = 1
         read(nulnam, nml = NAMSQLal, iostat = ierr )
@@ -384,7 +389,7 @@ contains
         if ( headerIndex > 1 .and. obsNlv > 0 ) &
           call sqlr_initHeader( obsdat, rdbSchema, familyType, headerIndex, elevReal, obsSat, azimuth, geoidUndulation, &
                                 earthLocRadCurv, roQcFlag, instrument, zenith, cloudCover, solarZenith, &
-                                solarAzimuth, landSea, obsIdo, xlat, xlon, codeType, obsDate, obsTime/100, obsStatus, idStation )
+                                solarAzimuth, landSea, obsIdo, xlat, xlon, codeType, obsDate, obsTime/100, obsStatus, idStation, id_prof )
         exit HEADER
       end if
 
@@ -473,6 +478,9 @@ contains
           call fSQL_get_column( stmt, COL_INDEX = 14, REAL8_VAR = azimuthReal )
           azimuth = nint( azimuthReal * 100 )
 
+        else if ( trim(rdbSchema)=='al' ) then
+          call fSQL_get_column( stmt, COL_INDEX = 10, INT_VAR   = id_prof )
+
         end if
 
       end if  ! TOVS or CONV
@@ -489,7 +497,7 @@ contains
 
           call sqlr_initHeader( obsdat, rdbSchema, familyType, headerIndex, elevReal, obsSat, azimuth, geoidUndulation, &
                                 earthLocRadCurv, roQcFlag, instrument, zenith, cloudCover, solarZenith, &
-                                solarAzimuth, landSea, obsIdo, xlat, xlon, codeType, obsDate, obsTime/100, obsStatus, idStation )
+                                solarAzimuth, landSea, obsIdo, xlat, xlon, codeType, obsDate, obsTime/100, obsStatus, idStation, id_prof )
           exit DATA
 
         else if ( int(matdata(rowIndex,2)) == obsIdo ) then
@@ -497,7 +505,7 @@ contains
           if ( headerIndex == headerIndexStart .and. obsNlv == 0  ) &
             call sqlr_initHeader( obsdat, rdbSchema, familyType, headerIndex, elevReal, obsSat, azimuth, geoidUndulation, &
                                   earthLocRadCurv, roQcFlag, instrument, zenith, cloudCover, solarZenith, &
-                                  solarAzimuth, landSea, obsIdo, xlat, xlon, codeType, obsDate, obsTime/100, obsStatus, idStation )
+                                  solarAzimuth, landSea, obsIdo, xlat, xlon, codeType, obsDate, obsTime/100, obsStatus, idStation, id_prof )
 
           lastId = rowIndex + 1
           obsIdd = int(matdata(rowIndex,1))
@@ -576,7 +584,7 @@ contains
         if ( lastId > numberRows ) &
           call sqlr_initHeader( obsdat, rdbSchema, familyType, headerIndex, elevReal, obsSat, azimuth, geoidUndulation, &
                                 earthLocRadCurv, roQcFlag, instrument, zenith, cloudCover, solarZenith, &
-                                solarAzimuth, landSea, obsIdo, xlat, xlon, codeType,obsDate, obsTime/100, obsStatus, idStation )
+                                solarAzimuth, landSea, obsIdo, xlat, xlon, codeType,obsDate, obsTime/100, obsStatus, idStation, id_prof )
 
       else
 
