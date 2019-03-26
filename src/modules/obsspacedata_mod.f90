743e1b495a6eda44c59a13ef3d90921a8cb27afe
Ervig Lapalme <ervig.lapalme@canada.ca>
2019-03-26 15:21:03 +0000

    Issue #189: Correct the format used in 'obs_enkf_prntbdy'
    The some integers were not formatted correctly which prevent those
    numbers to be printed correctly.

--- src/modules/obsspacedata_mod.f90
+++ src/modules/obsspacedata_mod.f90
@@ -3237,8 +3237,8 @@ contains
       else
          write(kulout,fmt=9100)idata,kstn
       end if
-9100  format(2x,'there are ',i3,1x,'data in record no.',1x,i6)
-9101  format(2x,'there is ',i3,1x,'data in record no.',1x,i6)
+9100  format(2x,'there are ',i6,1x,'data in record no.',1x,i6)
+9101  format(2x,'there is ', i6,1x,'data in record no.',1x,i6)
 
       ! print all data records
 
@@ -3270,11 +3270,10 @@ contains
             var3d, &
             obsdat%realBodies%columns(OBS_ZHA )%value_r(jdata), &
             obsdat%intBodies%columns(OBS_VCO )%value_i(jdata)
-
       enddo
 
-9201  format(1x,i3,1x,i6,1x,f7.0,1x,i3,10(1x,f10.3),1x,i2, &
-         1x,f10.3,1x,i2)
+9201  format(1x,i6,1x,i6,1x,f7.0,1x,i3,10(1x,f10.3),1x,i2, &
+           1x,f10.3,1x,i2)
 
       return
 
