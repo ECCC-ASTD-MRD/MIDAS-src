6cee05af238939e072dd739ad44bef94a7a1edf4
Mark Buehner <mark.buehner@canada.ca>
2019-09-13 19:45:13 +0000

    Issue #258: Make resources.def automatically system dependent
    The main resources.def file is set by a symbolic link by the
    script install_suite.sh. This means it has to be called
    before being able to compile (probably)! Some system tests have
    system specific resource settings now defined in resources.def

--- maestro/suites/midas_system_tests/install_suite.sh
+++ maestro/suites/midas_system_tests/install_suite.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+if [ "${TRUE_HOST}" = eccc-ppp1 -o "${TRUE_HOST}" = eccc-ppp2 ]; then
+    resources_file=resources.def_ppp12_xc40
+elif [ "${TRUE_HOST}" = eccc-ppp3 -o "${TRUE_HOST}" = eccc-ppp4 ]; then
+    resources_file=resources.def_ppp34_xc50
+else
+    echo "Unknown TRUE_HOST: ${TRUE_HOST}"
+    exit
+fi
+echo ${resources_file}
+cd resources
+rm -f resources.def
+ln -s ${resources_file} resources.def
+cd ../
+#exit
+
 SEQ_MAESTRO_SHORTCUT=${SEQ_MAESTRO_SHORTCUT:-". ssmuse-sh -d eccc/cmo/isst/maestro/1.5.3.3"}
 which getdef 1>/dev/null 2>&1 || ${SEQ_MAESTRO_SHORTCUT}
 
