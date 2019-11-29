b06fc8e868233b8f57a2544e6ea5d6888926faaa
Sylvain Heilliette <sylvain.heilliette@canada.ca>
2019-11-29 21:26:03 +0000

    Issue 282: bug corrected in make_src_files.sh

--- src/programs/make_src_files.sh
+++ src/programs/make_src_files.sh
@@ -89,6 +89,7 @@ for use1 in $uses1; do
                                   index17=${modulename_index[$use17]}
                                   for use18 in ${useslist[$index17]}; do
                                     level=18; levels[$level]="${levels[$level]} $use18"
+                                    index18=${modulename_index[$use18]}
                                     for use19 in ${useslist[$index18]}; do
                                     level=19; levels[$level]="${levels[$level]} $use19"
                                     echo " ERROR: More than 19 levels of dependencies found in program ${program}"
