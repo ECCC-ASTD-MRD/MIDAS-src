d7a439221201ccc519736b4aafa7b799cd92054f
Martin Deshaies-Jacques <martin.deshaies-jacques@canada.ca>
2020-05-26 20:18:28 +0000

    Issue #255: (#358) rm explicit double REAL stuff
    this a **gross** removal, finer simplifications will follow
    
    Issue #255: prepcma REAL8 + cleanup Makefile

--- src/target.mk
+++ src/target.mk
@@ -1,33 +1,13 @@
 ##=========================================================
-## 	Multi-real-precision/multi-architecture builds
+## 	Multi-architecture builds
 ##
-##	auto-precision for programs
-ifndef MIDAS_COMPILE_REAL_SINGLE
-    ifneq ($(filter obsIO.%,$(MAKECMDGOALS)),)
-        MIDAS_COMPILE_REAL_SINGLE = true
-        MAKECMDGOALS += 'MIDAS_COMPILE_REAL_SINGLE=true'
-    endif
-    ifneq ($(filter prepcma.%,$(MAKECMDGOALS)),)
-        MIDAS_COMPILE_REAL_SINGLE = true
-        MAKECMDGOALS += 'MIDAS_COMPILE_REAL_SINGLE=true'
-    endif
-endif
-##  Fix-hack for autocompletion for single precision programs
-obsIO.Abs: 
-prepcma.Abs:
-##
-##  **DO NOT CHANGE THESE `_DIR_SUF`**
 ##      (see http://make.mad-scientist.net/papers/multi-architecture-builds)
 ##
-ifeq ($(MIDAS_COMPILE_REAL_SINGLE), true)
-    _DIR_SUF := real4
-else
-    _DIR_SUF := real8
-endif
 
-DIR_TARGET := $(DIR_BLD_ROOT)/$(EC_ARCH)/$(_DIR_SUF)
 
 revnum := $(shell $(VERSION_SCRIPT))
+
+DIR_TARGET := $(DIR_BLD_ROOT)/$(revnum)/$(EC_ARCH)
 
 MAKETARGET = $(MAKE) --no-print-directory -C $@ -f $(CURDIR)/Makefile \
 				SRC_ROOT=$(CURDIR) VERSION=$(revnum) \
@@ -49,4 +29,4 @@ Makefile : ;
 
 % :: $(DIR_TARGET) ; :
 
-# vim: set noexpandtab noautoindent:
+# vim: set noexpandtab noautoindent nolist:
