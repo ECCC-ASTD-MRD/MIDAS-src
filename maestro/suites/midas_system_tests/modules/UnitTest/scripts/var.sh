977423c17d1ddd349a217164d438cf80f675d2b0
Ervig Lapalme <ervig.lapalme@canada.ca>
2020-06-12 19:23:55 +0000

    Propose some diagnostics in 'var.sh' to help find problems with RAM disk.
    They are introduced as comments so the developper can uncomment them
    when needed.

--- maestro/suites/midas_system_tests/modules/UnitTest/scripts/var.sh
+++ maestro/suites/midas_system_tests/modules/UnitTest/scripts/var.sh
@@ -17,7 +17,7 @@ if [ "${fasttmp}" = yes ]; then
     FASTTMPDIR=${ramdiskpath}/midas_${MP_CHILD}
     /bin/mkdir -p ${FASTTMPDIR}
     export MIDAS_RAMDISKDIR=${FASTTMPDIR}
-    /bin/df -hP ${FASTTMPDIR}
+    #echo "RAM disk space used on $(hostname) before pgm: $(/bin/df -hP ${FASTTMPDIR}) /bin/du -h ${FASTTMPDIR}"
 fi
 
 echo "The preparation of the working directory took ${SECONDS} seconds"
@@ -26,9 +26,21 @@ ulimit -c unlimited
 
 echo starting ${RUN_PGM} at $(${date_cmd} +%Y%m%d:%H:%M:%S.%N)
 SECONDS=0
-${RUN_PGM}
+
+#status=0
+#/usr/bin/time --format "Total Memory: %M Mb" ${RUN_PGM} || status=1
+${RUN_PGM} ## || status=1
+
 echo ending ${RUN_PGM} at $(${date_cmd} +%Y%m%d:%H:%M:%S.%N)
 echo "The program itself took ${SECONDS} seconds"
+
+#if [ "${fasttmp}" = yes ]; then
+#    ls -lRh ${FASTTMPDIR}
+#    echo "RAM disk space used on $(hostname) after pgm: $(/bin/df -hP ${FASTTMPDIR} | tail -1) /bin/du -h ${FASTTMPDIR}"
+#fi
+#if [ "${status}" -ne 0 ]; then
+#    exit 1
+#fi
 
 SECONDS=0
 
