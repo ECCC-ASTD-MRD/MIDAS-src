b74f6b92e9b8ec11b86673c54536b366720de31e
Ervig Lapalme <ervig.lapalme@canada.ca>
2020-06-25 18:50:25 +0000

    Issue #362: Add logic to 'wait_for_job.sh' to print the listing as it appears in the file

--- tools/misc/wait_for_job.sh
+++ tools/misc/wait_for_job.sh
@@ -16,23 +16,56 @@ jobid=${2}
 host=${3}
 sleep_interval=${4:-5}
 
-echo
-printf "\tWaiting for job '${jobid}' on host '${host}' with job '${jobname}' to finish\n"
-printf "\t...\n"
+logLine() {
+    set -e
+    printf "\t${*}\n"
+} ## End of function 'logLine'
 
+logLine
+logLine "Waiting for job '${jobid}' on host '${host}' with job '${jobname}' to finish"
+logLine
+
+findListing() {
+    set -e
+    __findListing_jobname__=${1}
+    __findListing_host__=${2}
+
+    /bin/ls -t ${__findListing_jobname__}.${__findListing_host__}-*-$(hostname)-*.out 2>/dev/null | head -1
+
+    unset __findListing_jobname__ __findListing_host__
+} ## End of function 'findListing'
+
+listing=$(findListing ${jobname} ${host} || true)
+
+linesListed=0
 while true; do
     status=0
     jobchk -c ${host} ${jobid} || status=$?
+
+    if [ -f "${listing}" ]; then
+        if [ "${linesListed}" -eq 0 ]; then
+            logLine ======================================
+            logLine "Begin of listing of '${jobname}'"
+            logLine
+        fi
+        let oldLines=linesListed+1
+        linesListed=$(cat ${listing} | wc -l)
+        ## Print from line '${oldLines}' to line '${linesListed}' in file '${listing}'
+        sed -n -e "${oldLines},${linesListed}p" ${listing}
+    else
+        listing=$(findListing ${jobname} ${host} || true)
+    fi
+
     if [ "${status}" -ne 0 ]; then
-        printf "\tThe job '${jobid}' on host '${host}' with job '${jobname}' has finished.\n"
-        listing=$(/bin/ls -t ${jobname}.${host}-*-$(hostname)-*.out | head -1)
         if [ -f "${listing}" ]; then
-            echo "======================================"
-            echo "Begin of listing of '${jobname}'"
-            cat ${listing}
+            let oldLines=linesListed+1
+            tail -n +${oldLines} ${listing}
             rm ${listing}
-            echo "End of listing of '${jobname}'"
-            echo "======================================"
+            logLine
+            logLine "End of listing of '${jobname}'"
+            logLine ======================================
+            logLine
+            logLine "The job '${jobid}' on host '${host}' with job '${jobname}' has finished."
         else
             echo "Cannot find listing: ${listing}"
         fi
