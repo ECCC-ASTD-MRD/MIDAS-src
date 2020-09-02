ccf8869dd0773bcce7c08a8e8a21286ff29fe404
Ervig Lapalme <ervig.lapalme@canada.ca>
2020-09-02 17:32:46 +0000

    Issue #382: Sur 'ubuntu-18.04' avec le profil '1.11.0', on doit loader 'rpn/code-tools/1.5.0'
    With '/fs/ssm/eccc/mrd/ordenv/profile/1.11.0', we must load ourselves
    'rpn/code-tools/1.5.0' when compiling on 'ubuntu-18.04-skylake-64'.

--- src/programs/commons/compile_setup.sh
+++ src/programs/commons/compile_setup.sh
@@ -47,6 +47,8 @@ elif [ "${ORDENV_PLAT}" = ubuntu-18.04-skylake-64 ]; then
     echo "... loading compiler hpco/exp/intelpsxe-cluster-19.0.3.199"
     . ssmuse-sh -d hpco/exp/intelpsxe-cluster-19.0.3.199
     LIBIRC=irc
+    echo "... loading code tools rpn/code-tools/1.5.0"
+    . ssmuse-sh -d eccc/mrd/rpn/code-tools/1.5.0
 elif [ "${ORDENV_PLAT}" = sles-11-amd64-64 -o "${ORDENV_PLAT}" = sles-11-broadwell-64-xc40 ]; then
     ## for s.compile, s.f90
     echo "... loading hpco/tmp/eccc/201402/07/base"
