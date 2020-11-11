0d69f4f392d523a9d10283aa083859c8e7cfb13c
David Lobon <david.lobon@canada.ca>
2020-11-11 10:24:11 -0500

    Issue #177 rebase Doc6

--- src/modules/sqlite_read_mod.f90
+++ src/modules/sqlite_read_mod.f90
@@ -570,6 +570,7 @@ contains
          call fSQL_get_column( stmt, COL_INDEX = 19, REAL_VAR  = obsrans)
          call fSQL_get_column( stmt, COL_INDEX = 18, REAL_VAR  = obsrane)
          call fSQL_get_column( stmt, COL_INDEX = 17, REAL_VAR  = obsrdel)
+         
       else  ! familyType = CONV
 
         call fSQL_get_column( stmt, COL_INDEX = 8,  INT_VAR  = obsStatus )
@@ -611,6 +612,7 @@ contains
                trackCellNum, modelWindSpeed,                                                    &
                real(obsrzam,kind=pre_obsReal), real(obsrele,kind=pre_obsReal),                  &
                real(obsrans,kind=pre_obsReal), real(obsrane,kind=pre_obsReal) , real(obsrdel,kind=pre_obsReal) )
+                                                          
           exit DATA
 
         else if ( int(matdata(rowIndex,2)) == obsIdo ) then
@@ -625,7 +627,8 @@ contains
                  trackCellNum, modelWindSpeed,                                                    &       
                  real(obsrzam,kind=pre_obsReal), real(obsrele,kind=pre_obsReal), &
                  real(obsrans,kind=pre_obsReal), real(obsrane,kind=pre_obsReal), real(obsrdel,kind=pre_obsReal)  )
-          end if
+                     
+           end if
 
           lastId = rowIndex + 1
           obsIdd = int(matdata(rowIndex,1))
