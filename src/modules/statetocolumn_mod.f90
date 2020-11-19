fef064db7be60c7a227d59e8b7dffba104c8a98e
David Lobon <david.lobon@canada.ca>
2020-11-19 15:06:59 -0500

    Issue #177 rebase

--- src/modules/statetocolumn_mod.f90
+++ src/modules/statetocolumn_mod.f90
@@ -356,6 +356,10 @@ contains
       doSlantPath = .true.
       SlantRO     = .true.
     endif
+    if ( slantPath_RA_nl   .and. inputStateVectorType == 'nl' ) then
+      doSlantPath = .true.
+      SlantRA     = .true.
+    endif
     write(*,*) 's2c_setupInterpInfo: doSlantPath, SlantTO, SlantRO, SlantRA = ', &
                doSlantPath, SlantTO, SlantRO, SlantRA
 
