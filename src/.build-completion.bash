0c0443e07c02ed9d588b12b1b98220731d2c606e
Martin Deshaies-Jacques <martin.deshaies-jacques@canada.ca>
2020-12-17 21:46:21 +0000

    Issue #443: minor fix completion targets

--- src/.build-completion.bash
+++ src/.build-completion.bash
@@ -7,10 +7,10 @@ _build_completions()
     fi
 
     
-    COMPREPLY+=($(compgen -W "$(cat Makefile | grep '^[1-9a-z_]*:' | cut -d':' -f1)" -- "${COMP_WORDS[1]}"))
+    COMPREPLY+=($(compgen -W "$(cat Makefile | grep '^[1-9a-z_.]*:' | cut -d':' -f1)" -- "${COMP_WORDS[1]}"))
     COMPREPLY+=($(compgen -W "$(ls programs/*.f90 |sed 's/programs\/\(.*\).f90/\1.Abs/')" -- "${COMP_WORDS[1]}"))
     COMPREPLY+=($(compgen -W "$(ls programs/*.f90 |sed 's/programs\/\(.*\).f90/\1.o/')" -- "${COMP_WORDS[1]}"))
-    COMPREPLY+=($(compgen -W "$(ls modules/*.f90 |sed 's/modules\/\(.*\)_mod.f90/\1.o/')" -- "${COMP_WORDS[1]}"))
+    COMPREPLY+=($(compgen -W "$(ls modules/*.f90 |sed 's/modules\/\(.*\).f90/\1.o/')" -- "${COMP_WORDS[1]}"))
 
 }
 complete -F _build_completions build_midas
