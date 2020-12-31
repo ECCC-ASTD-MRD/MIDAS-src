0780f8ae52a41184890a71953a6e04b92fdcc2aa
Martin Deshaies-Jacques <martin.deshaies-jacques@canada.ca>
2020-12-31 17:39:05 +0000

    Issue #255: prep for #447

--- src/config.dot.sh
+++ src/config.dot.sh
@@ -6,34 +6,35 @@
 ##
 ###########################################################
 set -x
-BACKEND=daley
-FRONTEND=eccc-ppp4
-JOBNAME=midasCompilation
-DIR_BLD_ROOT=${HOME}/data_maestro/ords/midas-bld
-DIR_BLD_LINK=../compiledir
-NCORES=8
-VERBOSE=2
-CLEAN=false
-DIRECT_FRONTEND_COMPILE=false
-FRONTEND_PLAT=ubuntu-18.04-skylake-64
+BACKEND=${BACKEND:-daley}
+FRONTEND=${FRONTEND:-eccc-ppp4}
+JOBNAME=${JOBNAME:-midasCompilation}
+DIR_BLD_ROOT=${DIR_BLD_ROOT:-${HOME}/data_maestro/ords/midas-bld}
+DIR_ABS=${DIR_ABS:-midas_abs}
+DIR_BLD_LINK=${DIR_BLD_LINK:-../compiledir}
+NCORES=${NCORES:-8}
+VERBOSE=${VERBOSE:-2}
+CLEAN=${CLEAN:-false}
+DIRECT_FRONTEND_COMPILE=${DIRECT_FRONTEND_COMPILE:-false}
+FRONTEND_PLAT=${FRONTEND_PLAT:-ubuntu-18.04-skylake-64}
 
 
 ###########################################################
 ##  SSM Packaging configuration 
 ##
-SSM_TARGET=/fs/ssm/eccc/mrd/rpn/anl/midas
-SSM_PKGNAME=midas
-SSM_MAINTAINER=ervig.lapalme@canada.ca
-SSM_DESCRIPTION="The Modular and Integrated Data Assimilation System"
-SSM_GITREPO=https://gitlab.science.gc.ca/atmospheric-data-assimilation/midas.git
-SSM_VERSION=3.6.0
+SSM_TARGET=${SSM_TARGET:-/fs/ssm/eccc/mrd/rpn/anl/midas}
+SSM_PKGNAME=${SSM_PKGNAME:-midas}
+SSM_MAINTAINER=${SSM_MAINTAINER:-ervig.lapalme@canada.ca}
+SSM_DESCRIPTION=${SSM_DESCRIPTION:-"The Modular and Integrated Data Assimilation System"}
+SSM_GITREPO=${SSM_GITREPO:-https://gitlab.science.gc.ca/atmospheric-data-assimilation/midas.git}
+SSM_VERSION=${SSM_VERSION:-3.6.0}
 
 
 ###########################################################
 ##  compilation and SSM needed for compilation
 ##
 ## -- should not change that
-DOT_CONFIG=${PWD}/programs/commons/compile_setup.sh
+__midas_dot_cfg=${PWD}/programs/commons/compile_setup.sh
 set +x
 
 ##  linking the build directory where it used to be
@@ -42,14 +43,14 @@ set +x
 || ln -s ${DIR_BLD_ROOT} ${DIR_BLD_LINK}
 
 ##  sourcing compilation configuration and SSM packages
-source ${DOT_CONFIG} || false
+source ${__midas_dot_cfg} || false
 
 
 ## check makedepf90 install
 if ! which makedepf90
 then 
     echo "<!> makedepf90 unavailable on the system."
-    echo "Check ${DOT_CONFIG} for proper SSM package"
+    echo "Check ${__midas_dot_cfg} for proper SSM package"
     false 
 fi
 
@@ -57,6 +58,7 @@ export BACKEND
 export FRONTEND
 export JOBNAME
 export DIR_BLD_ROOT
+export DIR_ABS
 export NCORES
 export VERBOSE
 export CLEAN
