ce08b815a4cf79bafa1531e5e4f97bef64718a8e
Alain Caya <alain.caya@canada.ca>
2021-02-11 22:41:23 +0000

    Issue #456 New surface variable LG
    The continuous ice concentration (LG) is added to the list of surface
    variables.

--- src/modules/varnamelist_mod.f90
+++ src/modules/varnamelist_mod.f90
@@ -43,7 +43,7 @@ module varNameList_mod
 
   ! These private parameters permit side-stepping a conflict with the Sphinx documenter,
   ! and an infinite loop
-  integer, parameter          :: VNLnumvarmax3D = 44,  VNLnumvarmax2D = 33,  VNLnumvarmaxOther = 5
+  integer, parameter          :: VNLnumvarmax3D = 44,  VNLnumvarmax2D = 34,  VNLnumvarmaxOther = 5
 
   integer, parameter          :: vnl_numvarmax3D    = VNLnumvarmax3D
   integer, parameter          :: vnl_numvarmax2D    = VNLnumvarmax2D
@@ -80,19 +80,19 @@ module varNameList_mod
                                  'P0  ','TG  ','UP  ','PB  ','ECO ','ENO2','EHCH','ESO2','ENH3', &
                                  'GL  ','WGE ','BIN ','MG  ','TM  ','QI1 ','QO1 ','STOR','ALFS', &
                                  'PN  ','PR  ','LPR ','I2  ','I3  ','I4  ','I5  ','I6  ','I8  ', &
-                                 'DN  ','FB  ','FI  ','MSKC','LZS ','WT  '/)
+                                 'DN  ','FB  ','FI  ','MSKC','LZS ','WT  ','LG  '/)
 
   character(len=4), parameter :: varLevelList2D(vnl_numvarmax2D) = (/    &
                                  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  &
                                  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  &
                                  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  &
-                                 'SF',  'SF',  'SF',  'SF',  'SF',  'SF'/)
+                                 'SF',  'SF',  'SF',  'SF',  'SF',  'SF',  'SF'/)
 
   character(len=2), parameter :: varKindList2D(vnl_numvarmax2D) = (/     &
                                  'MT',  'MT',  'MT',  'MT',  'CH',  'CH',  'CH',  'CH',  'CH', &
                                  'MT',  'MT',  'MT',  'MT',  'OC',  'HY',  'HY',  'HY',  'HY', &
                                  'MT',  'MT',  'MT',  'MT',  'MT',  'MT',  'MT',  'MT',  'MT', &
-                                 'MT',  'MT',  'MT',  'MT',  'HY',  'MT'/)
+                                 'MT',  'MT',  'MT',  'MT',  'HY',  'MT',  'MT'/)
 
   character(len=4), parameter :: vnl_varNameListOther(vnl_numvarmaxOther) = (/ &
                                  'I0  ','I1  ','I7  ','I9  ','SD  '/)
@@ -494,7 +494,7 @@ module varNameList_mod
         firstTime = .false.
         ! default values (not a valid variable name)
         forceSfcOnly(:) = 'XXXX'
-        
+
         if (utl_isNamelistPresent('namvnl','./flnml')) then
           nulnam = 0
           ierr = fnom(nulnam,'./flnml','FTN+SEQ+R/O',0)
