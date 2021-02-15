8217396bd88521646299c332ac670ba7861d816f
Martin Deshaies-Jacques <martin.deshaies-jacques@canada.ca>
2021-02-15 18:56:02 +0000

    Issue #457: addresses need for old and new env var
    @mab001 https://gitlab.science.gc.ca/atmospheric-data-assimilation/midas/issues/457#note_269497

--- src/programs/commons/retroComp_warning.sh
+++ src/programs/commons/retroComp_warning.sh
@@ -1,10 +1,55 @@
 #! /bin/sh
 
+typeset __do_retro=false
 
 ## **Temporary** retrocompatibility
-if [ ! -z ${COMPILE_MIDAS_ADD_DEBUG_OPTIONS+x} ] || \
-   [ ! -z ${COMPILEDIR_MIDAS_MAIN+x} ] || \
-   [ ! -z ${COMPILEDIR_MIDAS_ID+x} ] ;
+if [ ! -z ${COMPILE_MIDAS_ADD_DEBUG_OPTIONS+x} ]
+then
+    ## check if new variable also present and then if equal
+    if [ ! -z ${MIDAS_COMPILE_ADD_DEBUG_OPTIONS+x} ]; then
+        echo "Both MIDAS_COMPILE_ADD_DEBUG_OPTIONS and \
+COMPILE_MIDAS_ADD_DEBUG_OPTIONS defined"
+        if [ "${MIDAS_COMPILE_ADD_DEBUG_OPTIONS}" != "${COMPILE_MIDAS_ADD_DEBUG_OPTIONS}" ]; then
+            echo "... but have conflicting values! exiting."
+            exit 1
+        fi
+    else
+        __do_retro=true
+    fi
+fi
+
+
+if [ ! -z ${COMPILEDIR_MIDAS_MAIN+x} ]
+then
+    ## check if new variable also present and then if equal
+    if [ ! -z ${MIDAS_COMPILE_DIR_MAIN+x} ]; then
+        echo "Both MIDAS_COMPILE_DIR_MAIN and COMPILEDIR_MIDAS_MAIN defined"
+        if [ "${MIDAS_COMPILE_DIR_MAIN}" != "${COMPILEDIR_MIDAS_MAIN}" ]; then
+            echo "... but have conflicting values! exiting."
+            exit 1
+        fi
+    else
+        __do_retro=true
+    fi
+fi
+
+
+if [ ! -z ${COMPILEDIR_MIDAS_ID+x} ]
+then
+    ## check if new variable also present and then if equal
+    if [ ! -z ${MIDAS_COMPILE_DIR_ID+x} ]; then
+        echo "Both MIDAS_COMPILE_DIR_ID and COMPILEDIR_MIDAS_ID defined"
+        if [ "${MIDAS_COMPILE_DIR_ID}" != "${COMPILEDIR_MIDAS_ID}" ]; then
+            echo "... but have conflicting values! exiting."
+            exit 1
+        fi
+    else
+        __do_retro=true
+    fi
+fi
+
+
+if ${__do_retro}
 then
 
     cat << EndOfMessage
@@ -19,6 +64,8 @@ then
 ║  *  MIDAS_COMPILE_ADD_DEBUG_OPTIONS  <-  COMPILE_MIDAS_ADD_DEBUG_OPTIONS     ║
 ║  *  MIDAS_COMPILE_DIR_ID  <-  COMPILEDIR_MIDAS_ID                            ║
 ║                                                                              ║
+║  If both are present, they must be equals to prevent this message from       ║
+║  showing.  New variables have precedence.                                    ║
 ║                                                                              ║
 ║  Retro-compatibility is maintained **until** next release!                   ║
 ║                                                                              ║
