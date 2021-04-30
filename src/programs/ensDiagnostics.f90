77160e54e762d77fb02f5300a2f43cc5f694c290
Peter Houtekamer_-_Test_account <peter.houtekamer@canada.ca>
2021-04-30 14:15:12 +0000

    read the ensemble of trial field trajectories

--- src/programs/ensDiagnostics.f90
+++ src/programs/ensDiagnostics.f90
@@ -18,24 +18,33 @@ program midas_ensDiagnostics
   ! :Purpose: Compute diagnostics related to imbalance and spin-up in a data assimilation cycle     
   use version_mod
   use mpi_mod
+  use ensembleStateVector_mod
+  use gridStateVector_mod
+  use verticalCoord_mod
+  use horizontalCoord_mod
+  use analysisGrid_mod
   use timeCoord_mod
   use utilities_mod
   implicit none
 
+  type(struct_ens), pointer :: ensembleTrl
+  type(struct_vco), pointer :: vco_ens => null()
+  type(struct_hco), pointer :: hco_ens => null()
   character(len=256) :: ensPathName,ensFileName
   character(len=4) :: charmem
   integer, allocatable :: dateStampList(:)
   integer :: ierr, nulnam
-  integer, external :: fnom, fclos
+  integer, external :: fnom, fclos, fstopc
   integer :: iEns,nEns ! ensemble size
   character(len=12) :: prefix ! first part of input filenames. e.g. '2019061300'
-  character(len=12) :: postfix ! final part of input filenames. e.g. 'balance'
 
-  NAMELIST /namEnsDiagnostics/nEns,prefix,postfix
+  NAMELIST /namEnsDiagnostics/nEns,prefix
 
   call ver_printNameAndVersion('ensDiagnostics','Program to estimate imbalance in a model integration')
   call mpi_initialize
   write(*,*) 'hello from mpi-process: ',mpi_myid
+  ! Avoid printing lots of stuff to listing for std file I/O
+  ierr = fstopc('MSGLVL','ERRORS',0)
   !- Read the namelist
   nulnam = 0
   ierr = fnom(nulnam, './flnml', 'FTN+SEQ+R/O', 0)
@@ -48,7 +57,7 @@ program midas_ensDiagnostics
   iens=1
   write(charmem,'(I4.4)') iens
   ensPathName = '../input/inputs/'
-  ensFileName = trim(ensPathName)//trim(prefix)//charmem//trim(postfix)
+  ensFileName = trim(ensPathName)//trim(prefix)//charmem
   write(*,*) 'full input filename:',ensFileName 
   ierr = fclos(nulnam)
  
@@ -57,7 +66,25 @@ program midas_ensDiagnostics
   allocate(dateStampList(tim_nstepobsinc))
   call tim_getstamplist(dateStampList,tim_nstepobsinc,tim_getDatestamp())
   write(*,*) 'dateStamp of first time of trajectory: ',dateStampList(1)
-  write(*,*) 'dateStamp of last time of trajectory:  ',dateStampList(tim_nstepobsinc) 
+  write(*,*) 'dateStamp of last time of trajectory:  ',dateStampList(tim_nstepobsinc)
+
+  !- 2. Initialize variables and grids
+  call gsv_setup
+
+  call hco_SetupFromFile(hco_ens, ensFileName, ' ', 'ENSFILEGRID')
+  ! Note that the input file only contains P0 and PR fields, it does not have a vertical
+  ! grid descriptor.
+  call vco_setupFromFile(vco_ens, ensFileName)
+  ! PLH: not sure what agd_Setup does and if this is needed
+  call agd_SetupFromHCO(hco_ens)
+  ! Allocate ensembles, read the Trl ensemble
+  allocate(ensembleTrl)
+  ! PLH: need to make sure no interpolation will be done
+  call ens_allocate(ensembleTrl, nEns, tim_nstepobsinc, hco_ens, vco_ens, &
+                    dateStampList)
+  write(*,*) 'proper exit from ens_allocate'
+  call ens_readEnsemble(ensembleTrl, ensPathName, biPeriodic=.false.)
+  write(*,*) 'expecting issues with filenames prior to this'  
   call rpn_comm_finalize(ierr)
 
 end program midas_ensDiagnostics      
