2c23523874a33fcd76a1d486c041a32c33a24279
Maziar Bani Shahabadi <maziar.banishahabadi@canada.ca>
2021-05-29 02:21:35 +0000

    Issue #474: Remove extra stateVector allocation in inc_analPostProcessing.
    Calling gsv_readTrials with a double precision noZnoP stateVector
    for reading the trials in inc_analPostProcessing. The extra
    stateVector allocated to call gsv_readTrials for converting from
    single to double precision is removed.

--- src/modules/increment_mod.f90
+++ src/modules/increment_mod.f90
@@ -288,7 +288,6 @@ CONTAINS
     type(struct_gsv), intent(out) :: stateVectorAnal
 
     ! Locals:
-    type(struct_gsv)          :: stateVectorTrial_r4
     type(struct_vco), pointer :: vco_trl => null()
     type(struct_hco), pointer :: hco_trl => null()
 
@@ -309,21 +308,13 @@ CONTAINS
       allocHeightSfc = .true.
     end if
 
-    call gsv_allocate( stateVectorTrial_r4, tim_nstepobsinc, hco_trl, vco_trl,  &
-                       dateStamp_opt=tim_getDateStamp(), mpi_local_opt=.true., &
-                       mpi_distribution_opt='Tiles', dataKind_opt=4, &
-                       allocHeightSfc_opt=allocHeightSfc, hInterpolateDegree_opt='LINEAR', &
-                       beSilent_opt=.false. )
-    call gsv_zero( stateVectorTrial_r4 )
-    call gsv_readTrials( stateVectorTrial_r4 )
-
     call gsv_allocate( stateVectorTrial, tim_nstepobsinc, hco_trl, vco_trl,  &
                        dataKind_opt=pre_incrReal, &
                        dateStamp_opt=tim_getDateStamp(), mpi_local_opt=.true., &
                        allocHeightSfc_opt=allocHeightSfc, hInterpolateDegree_opt='LINEAR', &
                        allocHeight_opt=.false., allocPressure_opt=.false. )
-    call gsv_copy( stateVectorTrial_r4, stateVectorTrial, allowVarMismatch_opt=.true. )
-    call gsv_deallocate( stateVectorTrial_r4 )
+    call gsv_zero( stateVectorTrial )
+    call gsv_readTrials( stateVectorTrial )
     write(*,*) 'Memory Used: ',get_max_rss()/1024,'Mb'
 
     if (vco_trl%Vcode == 0 .or. .not. gsv_varExist(varName='P0')) then
