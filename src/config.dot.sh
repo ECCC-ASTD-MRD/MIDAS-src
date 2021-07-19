73df9f1b86ac31e052db4ff9fe438ab41e24759a
Sylvain Heilliette <syh074@science.gc.ca>
2021-07-19 16:00:01 +0000

    Issue #508: introduction of modified ssm to test RTTOV-13 in place of RTOV-12

--- src/config.dot.sh
+++ src/config.dot.sh
@@ -191,9 +191,9 @@ if [ "${ORDENV_PLAT}" = ubuntu-18.04-skylake-64 -o "${ORDENV_PLAT}" = sles-15-sk
         . ssmuse-sh -x main/opt/perftools/perftools-2.0/${COMP_ARCH}
     fi
 
-    echo "... loading eccc/mrd/rpn/anl/rttov/12v1.5.1/${COMP_ARCH}"
-    . r.load.dot eccc/mrd/rpn/anl/rttov/12v1.5.1/${COMP_ARCH}
-
+    echo "... loading /home/syh074/SSM/arma/rttov_test//${COMP_ARCH}"
+    . r.load.dot /home/syh074/SSM/arma/rttov_test/${COMP_ARCH}
+    
     ## for 'random_tools'
     echo "... loading eccc/mrd/rpn/anl/random_tools/Release_1.0.0-HPCRU1"
     . ssmuse-sh -d eccc/mrd/rpn/anl/random_tools/Release_1.0.0-HPCRU1
