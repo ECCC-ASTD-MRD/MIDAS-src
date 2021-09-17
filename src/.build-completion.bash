fd233dc3b45065917fd639fdaa16863fe6bba7df
Martin Deshaies-Jacques <martin.deshaies-jacques@canada.ca>
2021-09-17 15:26:00 +0000

    Issue #490: uniform .Abs target + autocomp fix

--- src/.build-completion.bash
+++ src/.build-completion.bash
@@ -6,6 +6,7 @@ _build_completions()
     COMPREPLY+=($(compgen -W "$(ls programs/*.f90 |sed 's/programs\/\(.*\).f90/\1.Abs/')" -- "${COMP_WORDS[COMP_CWORD]}"))
     COMPREPLY+=($(compgen -W "$(ls programs/*.f90 |sed 's/programs\/\(.*\).f90/\1.o/')" -- "${COMP_WORDS[COMP_CWORD]}"))
     COMPREPLY+=($(compgen -W "$(ls modules/*.f90 |sed 's/modules\/\(.*\).f90/\1.o/')" -- "${COMP_WORDS[COMP_CWORD]}"))
+    COMPREPLY+=($(compgen -W "splitobs.Abs" -- "${COMP_WORDS[COMP_CWORD]}"))
 
 }
 complete -F _build_completions ./midas_build
