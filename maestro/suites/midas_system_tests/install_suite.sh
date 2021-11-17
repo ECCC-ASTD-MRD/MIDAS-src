2c8957aaccf7a55c8462ffb32ee95b571ec8ffe5
Ervig Lapalme <ervig.lapalme@canada.ca>
2021-11-17 21:19:05 +0000

    Issue #493: Add variable 'MIDAS_toplevel' in 'abs.dot' to be able to guide users to the source code.

--- maestro/suites/midas_system_tests/install_suite.sh
+++ maestro/suites/midas_system_tests/install_suite.sh
@@ -121,8 +121,9 @@ if [[ ${MIDAS_TESTS_SUITE} = */* ]]; then
     mkdir -p $(dirname ${MIDAS_TESTS_SUITE})
 fi
 
-echo "ABS_DIR=${MIDAS_COMPILE_DIR_MAIN:-${__toplevel}/compiledir}/midas_abs" > abs.dot
-echo "MIDAS_version=\$(cd ${__toplevel}; ./midas.version.sh)" >> abs.dot
+echo "MIDAS_toplevel=${__toplevel}" >> abs.dot
+echo "ABS_DIR=${MIDAS_COMPILE_DIR_MAIN:-${__toplevel}/compiledir}/midas_abs" >> abs.dot
+echo "MIDAS_version=\$(cd \${MIDAS_toplevel}; ./midas.version.sh)" >> abs.dot
 if [ -n "${MIDAS_ABS}" ]; then
     . ./abs.dot
     mkdir -p ${ABS_DIR}
