8f413f065a6df6f6636bb7c268f24c866084471b
Mark Buehner <mark.buehner@canada.ca>
2022-03-01 19:50:52 +0000

    Issue #594: Several changes from #559 that were missing in main

--- maestro/suites/midas_system_tests/modules/UnitTest/scripts/var.sh
+++ maestro/suites/midas_system_tests/modules/UnitTest/scripts/var.sh
@@ -18,13 +18,6 @@ if [ "${fasttmp}" = yes ]; then
     [ -d "${FASTTMPDIR}" ] && /bin/rm -rf ${FASTTMPDIR}
     /bin/mkdir -p ${FASTTMPDIR}
     export MIDAS_RAMDISKDIR=${FASTTMPDIR}
-    set +x
-    free -h
-    echo "RAM disk space used on $(hostname) before pgm: "
-    echo "     $(/bin/df -hP ${FASTTMPDIR})"
-    echo "     $(/bin/du -h ${FASTTMPDIR})"
-    ls -lR /tmp || true
-    set -x
 fi
 
 echo "The preparation of the working directory took ${SECONDS} seconds"
@@ -34,22 +27,12 @@ ulimit -c unlimited
 echo starting ${RUN_PGM} at $(${date_cmd} +%Y%m%d:%H:%M:%S.%N)
 SECONDS=0
 
-status=0
+#status=0
 #/usr/bin/time --format "Total Memory: %M Mb" ${RUN_PGM} || status=1
-${RUN_PGM} || status=1  ## Capture the status to inspect the directory before aborting later
+${RUN_PGM}
 
 echo ending ${RUN_PGM} at $(${date_cmd} +%Y%m%d:%H:%M:%S.%N)
 echo "The program itself took ${SECONDS} seconds"
-
-if [ "${fasttmp}" = yes ]; then
-    ls -lRh ${FASTTMPDIR}
-    echo "RAM disk space used on $(hostname) after pgm: "
-    echo "     $(/bin/df -hP ${FASTTMPDIR})"
-    echo "     $(/bin/du -h ${FASTTMPDIR})"
-fi
-if [ "${status}" -ne 0 ]; then
-    exit 1
-fi
 
 SECONDS=0
 
