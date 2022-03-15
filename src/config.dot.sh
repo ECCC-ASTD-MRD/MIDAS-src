df4c8d74a5bd3f35fcb44afd9596e8a705db713a
Sylvain Heilliette <sylvain.heilliette@canada.ca>
2022-03-15 15:11:36 +0000

    Issue #508: changed to official RTTOV-13 library (ervig local version) + corresponding changes to system tests namelists and inputs

--- src/config.dot.sh
+++ src/config.dot.sh
@@ -190,10 +190,10 @@ if [ "${ORDENV_PLAT}" = ubuntu-18.04-skylake-64 -o "${ORDENV_PLAT}" = sles-15-sk
         echo "... loading main/opt/perftools/perftools-2.0/${COMP_ARCH}"
         . ssmuse-sh -x main/opt/perftools/perftools-2.0/${COMP_ARCH}
     fi
-    
-    echo "... loading  /home/syh074/SSM/arma/rttov/${COMP_ARCH}"
-    . r.load.dot -d /home/syh074/SSM/arma/rttov/${COMP_ARCH} 
-    
+ 
+    echo "... loading /home/erv000/data/ords/SSM/rttov-13/1.1/${COMP_ARCH}"
+    . r.load.dot -d /home/erv000/data/ords/SSM/rttov-13/1.1/${COMP_ARCH}
+	
     ## for 'random_tools'
     echo "... loading eccc/mrd/rpn/anl/random_tools/Release_1.0.0-HPCRU1"
     . ssmuse-sh -d eccc/mrd/rpn/anl/random_tools/Release_1.0.0-HPCRU1
