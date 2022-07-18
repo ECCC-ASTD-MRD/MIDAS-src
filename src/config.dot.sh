8256849fca6f485d2f7a8cea55f857c122038657
Martin Deshaies-Jacques <martin.deshaies-jacques@canada.ca>
2022-07-18 19:59:01 +0000

    Issue #671: makedepf90 timeout

--- src/config.dot.sh
+++ src/config.dot.sh
@@ -39,11 +39,12 @@ MIDAS_SSM_VERSION=${MIDAS_SSM_VERSION:-${__revnum}}
 ##  it can impact the maestro testing suite or the cleaning targets
 ##  in unwated ways
 MIDAS_ABS_LEAFDIR=${MIDAS_ABS_LEAFDIR:-midas_abs}
+MIDAS_MAKEDEP_TIMEOUT=${MIDAS_MAKEDEP_TIMEOUT:-5s}
 __install_always_midas=true
 __compiledir_link=${__compiledir_link:-${__toplevel}/compiledir}
 __build_dir_version=${MIDAS_COMPILE_DIR_MAIN}/midas_bld-${__revstring}
 __keep_jobsubmit_ofile=false
-__ordsoumet_wallclock=${__ordsoumet_wallclock:-20}
+__ordsoumet_wallclock=${__ordsoumet_wallclock:-10}
 
 ##  linking the build directory where it used to be
 ([ -d  ${__compiledir_link} ]||[ -L ${__compiledir_link} ]) \
@@ -271,6 +272,7 @@ export MIDAS_COMPILE_FRONTEND
 export MIDAS_COMPILE_JOBNAME
 export MIDAS_ABS_LEAFDIR
 export MIDAS_COMPILE_VERBOSE
+export MIDAS_MAKEDEP_TIMEOUT
 
 export MIDAS_SSM_TARGET
 export MIDAS_SSM_PKGNAME
