ff5af350470c7bf6fc7f36e6874a06f04e347fd9
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2022-07-26 13:56:05 +0000

    Issue #669: Add print for memory used 'thn_QsortInt_withHoles'

--- src/modules/thinning_mod.f90
+++ src/modules/thinning_mod.f90
@@ -4000,6 +4000,9 @@ write(*,*) 'Setting bit 11 for codtyp, elem = ', codtyp, obsVarNo
       end if
     end do
 
+    write(*,*) 'thn_QsortInt_withHoles: numSelected size(A)', numSelected, size(A)
+    write(*,*) 'thn_QsortInt_withHoles: before allocate Memory Used: ',get_max_rss()/1024,'Mb'
+
     allocate(buffer(numSelected))
     allocate(indicesInBuffer(numSelected))
     allocate(indices(size(A)))
@@ -4013,6 +4016,8 @@ write(*,*) 'Setting bit 11 for codtyp, elem = ', codtyp, obsVarNo
       end if
       indices(index) = index
     end do
+
+    write(*,*) 'thn_QsortInt_withHoles: after allocate Memory Used: ',get_max_rss()/1024,'Mb'
 
     call thn_QsortInt(buffer,indicesInBuffer)
 
@@ -4029,10 +4034,13 @@ write(*,*) 'Setting bit 11 for codtyp, elem = ', codtyp, obsVarNo
       end if
     end do
 
+    write(*,*) 'thn_QsortInt_withHoles: before deallocate Memory Used: ',get_max_rss()/1024,'Mb'
+
     deallocate(buffer)
     deallocate(indices)
     deallocate(indicesInBuffer)
 
+    write(*,*) 'thn_QsortInt_withHoles: after deallocate Memory Used: ',get_max_rss()/1024,'Mb'
   end subroutine thn_QsortInt_withHoles
 
   !--------------------------------------------------------------------------
