32b318e8188f629e72dbf6e177d93b4413e30a46
Arlan <arlan.dirkson@ec.gc.ca>
2022-09-12 21:14:51 +0000

    Issue #693:
    - Bug addressed by
          - adding `OBS_OMAM` as a real-body column number in `obsspacedata_mod`,
          - replacing `OBS_WORK` with `OBS_OMAM` in `letkf`.
    - Behaviour now as expected w.r.t. diag files, having tested different
    observation errors for UA (original and x1000)
    - `lam_ppp` test fails as expected based on *.not_validating, only affecting diag files.
    - All remaining tests pass.

--- src/modules/obsspacedata_mod.f90
+++ src/modules/obsspacedata_mod.f90
@@ -612,8 +612,9 @@ module ObsColumnNames_mod
    integer, parameter, public :: OBS_SEM = OBS_PPP +1 ! surface emissivity
    integer, parameter, public :: OBS_VAR = OBS_SEM +1 ! value of the observation
    integer, parameter, public :: OBS_OMP = OBS_VAR +1 ! obs - H (trial field)
-   integer, parameter, public :: OBS_OMA = OBS_OMP +1 ! obs - H (analysis)
-   integer, parameter, public :: OBS_OER = OBS_OMA +1 ! sigma(obs)
+   integer, parameter, public :: OBS_OMA = OBS_OMP +1 ! obs - H (analysis mean)
+   integer, parameter, public :: OBS_OMAM= OBS_OMA +1 ! obs - H (analysis member)
+   integer, parameter, public :: OBS_OER = OBS_OMAM +1 ! sigma(obs)
    integer, parameter, public :: OBS_HPHT= OBS_OER +1 ! root of (hpht with hx scalar)
    integer, parameter, public :: OBS_HAHT= OBS_HPHT +1 ! root of (hp_{a}ht with hx scalar)
    integer, parameter, public :: OBS_ZHA = OBS_HAHT+1 ! vert coordinate for Schur product
@@ -634,7 +635,7 @@ module ObsColumnNames_mod
    integer, parameter, public :: OBS_LOND= OBS_LATD+1 ! obs LONgitude in Data table (radians)
    integer, parameter, public :: OBS_BTCL= OBS_LOND+1 ! clear-sky simulated observation
    integer, parameter, public :: OBS_LOCI= OBS_BTCL+1 ! LOCation Information for observation (e.g. range along radar beam)
-
+   
    ! the number of real body variables defined just above
    integer, parameter :: NBDY_REAL_END = OBS_LOCI
    integer, parameter :: NBDY_REAL_SIZE = NBDY_REAL_END - NBDY_REAL_BEG + 1
@@ -643,9 +644,9 @@ module ObsColumnNames_mod
    ! REAL-BODY COLUMN NAMES
    !
    character(len=4), target :: ocn_ColumnNameList_RB(NBDY_REAL_BEG:NBDY_REAL_END) = &
-      (/ 'PPP ','SEM ','VAR ','OMP ','OMA ','OER ','HPHT','HAHT','ZHA ','OMP6','OMA0',     &
-         'SIGI','SIGO','POB ','WORK','PRM ','JOBS','QCV ','FSO ','CRPS','BCOR','OMPE',     &
-         'ROLA','ROLO','VAR2','LOCI' /)
+      (/ 'PPP ','SEM ','VAR ','OMP ','OMA ','OMAM','OER ','HPHT','HAHT','ZHA ','OMP6',     &
+         'OMA0','SIGI','SIGO','POB ','WORK','PRM ','JOBS','QCV ','FSO ','CRPS','BCOR',     &
+         'OMPE','ROLA','ROLO','VAR2','LOCI' /)
 end module ObsColumnNames_mod
 
 
@@ -1059,9 +1060,9 @@ contains
               OBS_XTR, OBS_QCF2, OBS_CLA, (0,ii=10,100) /)
 
          bdy_real_column_list= &
-            (/OBS_PPP, OBS_SEM, OBS_VAR, OBS_OMP, OBS_OMA, OBS_OER, OBS_HPHT,&
+            (/OBS_PPP, OBS_SEM, OBS_VAR, OBS_OMP, OBS_OMA, OBS_OMAM, OBS_OER, OBS_HPHT,&
               OBS_HAHT,OBS_ZHA, OBS_OMP6, OBS_OMA0, OBS_SIGI, OBS_SIGO, OBS_WORK, OBS_PRM,&
-              OBS_JOBS, OBS_BCOR,OBS_LOND,OBS_LATD, (0,ii=20,100) /)
+              OBS_JOBS, OBS_BCOR,OBS_LOND,OBS_LATD, (0,ii=21,100) /)
 
          do list_index=1,COLUMN_LIST_SIZE
             column_index = hdr_int_column_list(list_index)
@@ -1510,10 +1511,10 @@ module ObsSpaceData_mod
    public :: OBS_XTR, OBS_QCF2, OBS_CLA
 
    !    real-body column numbers
-   public :: OBS_PPP, OBS_SEM, OBS_VAR, OBS_OMP, OBS_OMA, OBS_OER, OBS_HPHT
-   public :: OBS_HAHT,OBS_ZHA, OBS_OMP6,OBS_OMA0,OBS_SIGI,OBS_SIGO,OBS_POB
-   public :: OBS_WORK,OBS_PRM, OBS_JOBS,OBS_QCV, OBS_FSO, OBS_CRPS,OBS_BCOR
-   public :: OBS_OMPE,OBS_LATD,OBS_LOND,OBS_BTCL,OBS_LOCI
+   public :: OBS_PPP, OBS_SEM, OBS_VAR, OBS_OMP, OBS_OMA, OBS_OMAM, OBS_OER
+   public :: OBS_HPHT,OBS_HAHT,OBS_ZHA, OBS_OMP6,OBS_OMA0,OBS_SIGI, OBS_SIGO
+   public :: OBS_WORK,OBS_PRM, OBS_JOBS,OBS_QCV, OBS_FSO, OBS_CRPS, OBS_BCOR
+   public :: OBS_POB, OBS_OMPE,OBS_LATD,OBS_LOND,OBS_BTCL,OBS_LOCI
 
    ! OBSERVATION-SPACE FUNDAMENTAL PARAMETERS
    integer, public, parameter :: obs_assimilated    = 1 ! OBS_ASS value for assimilated obs
