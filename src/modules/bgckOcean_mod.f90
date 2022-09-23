9cf2b2d2ff6b5a28fedfdda5f8594acac93f71f8
Sergey Skachko <Sergey.Skachko@canada.ca>
2022-09-23 15:05:26 +0000

    Issue#679: required change after rebase

--- src/modules/bgckOcean_mod.f90
+++ src/modules/bgckOcean_mod.f90
@@ -112,7 +112,7 @@ module bgckOcean_mod
     
     ! Read the namelist
     if (.not. utl_isNamelistPresent('namOceanBGcheck','./flnml')) then
-      if (mpi_myid == 0) then
+      if (mmpi_myid == 0) then
         write(*,*) 'ocebg_bgCheckSST: namOceanBGcheck is missing in the namelist.'
         write(*,*) 'ocebg_bgCheckSST: the default values will be taken.'
       end if
