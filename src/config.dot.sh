334b1b1e5550d64d7125dcc71b59295a7be28c6a
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2023-06-06 14:47:17 +0000

    Issue #819: Using 'rpn/{libs,utils}/20230531'

--- src/config.dot.sh
+++ src/config.dot.sh
@@ -138,8 +138,8 @@ HDF5_LIBS="hdf5hl_fortran hdf5_hl hdf5_fortran hdf5 z"
 
 ## for rmn, vgrid, rpncomm
 VGRID_LIBNAME="vgrid"
-echo "... loading eccc/mrd/rpn/libs/20230414"
-. r.load.dot eccc/mrd/rpn/libs/20230414
+echo "... loading eccc/mrd/rpn/libs/20230531"
+. r.load.dot eccc/mrd/rpn/libs/20230531
 echo "... loading hdf5"
 . ssmuse-sh -d main/opt/hdf5-netcdf4/serial/static/${COMP_ARCH}/01
 
