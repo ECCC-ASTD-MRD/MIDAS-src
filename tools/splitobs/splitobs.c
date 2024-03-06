2591f3d1b5654f88c9224c175015b2015ae0b120
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2024-03-06 18:05:22 +0000

    Issue #908: We do not need to set ourselves the definitions 'WKF_*' since they are provided by 'rmn.h'.

--- tools/splitobs/splitobs.c
+++ tools/splitobs/splitobs.c
@@ -256,48 +256,6 @@ int main(int argc, char** argv) {
     exit_program(NOT_OK,PROGRAM_NAME,PROBLEM,VERSION);
   }
 
-  /* Selon le code dans RMNLIB pour 'wkoffit.c', le chiffre qui sort de 'c_wkoffit' correspond a ceci: */
-#define WKF_INEXISTANT            -3
-#define WKF_VIDE                  -2
-#define WKF_INCONNU               -1
-#define WKF_RANDOM89               1
-#define WKF_SEQUENTIEL89           2
-#define WKF_SEQUENTIELFORTRAN89    3
-#define WKF_CCRN                   4
-#define WKF_CCRN_RPN               5
-#define WKF_BURP                   6
-#define WKF_GRIB                   7
-#define WKF_BUFR                   8
-#define WKF_BLOK                   9
-#define WKF_FORTRAN               10
-#define WKF_COMPRESS              11
-#define WKF_GIF89                 12
-#define WKF_GIF87                 13
-#define WKF_IRIS                  14
-#define WKF_JPG                   15
-#define WKF_KMW                   16
-#define WKF_PBM                   17
-#define WKF_PCL                   18
-#define WKF_PCX                   19
-#define WKF_PDSVICAR              20
-#define WKF_PM                    21
-#define WKF_PPM                   22
-#define WKF_PS                    23
-#define WKF_KMW_                  24
-#define WKF_RRBX                  25
-#define WKF_SUNRAS                26
-#define WKF_TIFF                  27
-#define WKF_UTAHRLE               28
-#define WKF_XBM                   29
-#define WKF_XWD                   30
-#define WKF_ASCII                 31
-#define WKF_BMP                   32
-#define WKF_RANDOM98              33
-#define WKF_SEQUENTIEL98          34
-#define WKF_NETCDF                35
-/* Cette definition est un ajout */
-#define WKF_SQLite                36
-
   status = access(opt.obsin,F_OK);
   if ( status != 0 ) { /* Le fichier existe deja */
       fprintf(stderr,"The file '%s' does not exist and should.\n",opt.obsin);
