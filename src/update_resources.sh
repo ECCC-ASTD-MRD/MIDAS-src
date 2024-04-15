3f2a06a3cdafa79fe528d0292101c7d0822f655f
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2024-04-15 14:35:37 +0000

    Issue #900: The 'src/update_resources.sh' now udpates resources back and forth.
    The script 'src/update_resources.sh' previously was only turning
    **off** the 'check' and 'clean' tasks.  But now, we want to the turn
    **on** the tasks if they have been shut off previously if the
    compilation options have been changed.

--- src/update_resources.sh
+++ src/update_resources.sh
@@ -6,14 +6,24 @@ typeset -r toplevel=$(git rev-parse --show-toplevel)
 
 ## Check if any of 'MIDAS_COMPILE_ADD_DEBUG_OPTIONS' or
 ## 'MIDAS_COMPILE_CODECOVERAGE_DATAPATH' is set.
-## If not set, just do nothing!
-if [ "${MIDAS_COMPILE_ADD_DEBUG_OPTIONS:-}" != yes -a -z "${MIDAS_COMPILE_CODECOVERAGE_DATAPATH:-}" ]; then
-    exit
+if [ "${MIDAS_COMPILE_ADD_DEBUG_OPTIONS:-}" = yes -o -n "${MIDAS_COMPILE_CODECOVERAGE_DATAPATH:-}" ]; then
+    ## If any of these environment variables above is set, we want
+    ## avoid running the 'check' and 'clean' tasks in the 'UnitTest'
+    ## module by setting 'CHECK_RESULTS_CATCHUP' and
+    ## 'CHECK_RESULTS_CATCHUP' to '9' in
+    ## 'maestro/suites/midas_system_tests/resources/resources.def'
+    typeset -r check_result_catchup_expected_value=9
+    typeset -r clean_unittest_catchup_expected_value=9
+else
+    ## If any of these environment variables above is NOT set, we want
+    ## to run the 'check' and 'clean' tasks in the 'UnitTest' module
+    ## so we set 'CHECK_RESULTS_CATCHUP' and 'CHECK_RESULTS_CATCHUP'
+    ## to '2' in
+    ## 'maestro/suites/midas_system_tests/resources/resources.def'
+    typeset -r check_result_catchup_expected_value=2
+    typeset -r clean_unittest_catchup_expected_value=2
 fi
 
-## If any of these variables is set, we avoid running the 'check'
-## and 'clean' tasks in the 'UnitTest' module by setting
-## 'CHECK_RESULTS_CATCHUP' and 'CHECK_RESULTS_CATCHUP' to '9'
 
 typeset -r resources_file=${toplevel}/maestro/suites/midas_system_tests/resources/resources.def
 ## If the resources file does not exist, then the user has not yet
@@ -38,24 +48,27 @@ if [ "$(grep -c '^CLEAN_UNITTEST_CATCHUP=' ${resources_file})" -ne 1 ]; then
 fi
 
 ## If the variables 'CHECK_RESULTS_CATCHUP' and
-## 'CLEAN_UNITTEST_CATCHUP' both have already a value of '9', then
+## 'CLEAN_UNITTEST_CATCHUP' both have already the expected value, then
 ## we don't need to change the file
 typeset -r check_result_catchup=$(awk -F= '/^CHECK_RESULTS_CATCHUP=/ {print $2}' ${resources_file})
 typeset -r clean_unittest_catchup=$(awk -F= '/^CLEAN_UNITTEST_CATCHUP=/ {print $2}' ${resources_file})
-if [ "${check_result_catchup}"  -eq 9 -a "${clean_unittest_catchup}" -eq 9 ]; then
-    echo "Both variables 'CHECK_RESULTS_CATCHUP' and 'CLEAN_UNITTEST_CATCHUP' are already set to 9 in '${resources_file}'"
+if [ "${check_result_catchup}"   -eq "${check_result_catchup_expected_value}" -a \
+     "${clean_unittest_catchup}" -eq "${clean_unittest_catchup_expected_value}" ]; then
+    echo "Both variables 'CHECK_RESULTS_CATCHUP' and 'CLEAN_UNITTEST_CATCHUP' are already set"
+    echo "to ${check_result_catchup_expected_value} and ${clean_unittest_catchup_expected_value}"
+    echo "respectively in '${resources_file}'."
     echo "We will not modify the resources file"
     exit
 fi
 
 ## If 'CHECK_RESULTS_CATCHUP' is not set to 9, then update it
-if [ "${check_result_catchup}" -ne 9 ]; then
-    echo "Set 'CHECK_RESULTS_CATCHUP=9' in ${resources_file}"
-    sed -i 's/^CHECK_RESULTS_CATCHUP=[1-8]$/CHECK_RESULTS_CATCHUP=9/' ${resources_file}
+if [ "${check_result_catchup}" -ne "${check_result_catchup_expected_value}" ]; then
+    echo "Set 'CHECK_RESULTS_CATCHUP=${check_result_catchup_expected_value}' in ${resources_file}"
+    sed -i "s/^CHECK_RESULTS_CATCHUP=[1-9]$/CHECK_RESULTS_CATCHUP=${check_result_catchup_expected_value}/" ${resources_file}
 fi
 
 ## If 'CLEAN_UNITTEST_CATCHUP' is not set to 9, then update it
-if [ "${clean_unittest_catchup}" -ne 9 ]; then
-    echo "Set 'CLEAN_UNITTEST_CATCHUP=9' in ${resources_file}"
-    sed -i 's/^CLEAN_UNITTEST_CATCHUP=[1-8]$/CLEAN_UNITTEST_CATCHUP=9/' ${resources_file}
+if [ "${clean_unittest_catchup}" -ne "${clean_unittest_catchup_expected_value}" ]; then
+    echo "Set 'CLEAN_UNITTEST_CATCHUP=${clean_unittest_catchup_expected_value}' in ${resources_file}"
+    sed -i "s/^CLEAN_UNITTEST_CATCHUP=[1-9]$/CLEAN_UNITTEST_CATCHUP=${clean_unittest_catchup_expected_value}/" ${resources_file}
 fi
