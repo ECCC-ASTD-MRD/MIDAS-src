ab6b2686b46905e210582c14bdf8a0fe27ff4bdf
Laurence Coursol <laurence.coursol@ec.gc.ca>
2024-05-16 21:22:30 +0000

    Issue #904: post rebase adjustment related to inn_getHcoVcoFromTrlmFile interface change

--- src/programs/dfs.f90
+++ src/programs/dfs.f90
@@ -286,7 +286,7 @@ program midas_dfs
   call var_setup('VAR') ! obsColumnMode
 
   ! Reading trials
-  call inn_getHcoVcoFromTrlmFile(hco_trl, vco_trl)
+  call inn_getHcoVcoFromTrlmFile(hco_trl, vco_trl, './trlm_01')
   allocHeightSfc = ( vco_trl%Vcode /= 0 )
 
   call gsv_allocate(stateVectorTrialHighRes, tim_nstepobs, hco_trl, vco_trl,  &
