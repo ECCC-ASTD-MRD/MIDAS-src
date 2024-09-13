9d0f4d6a71676e4381d197c56fa3611d80e05ae1
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2024-09-13 14:08:04 +0000

    Issue #969: Fix the compilation warnings in 'oceanObservations_mod'
    There remain this warning message:
            src/programs/prepcma.f90(1003): warning #6182: Fortran 2008 does not allow this edit descriptor.   [I]
                        write(*,'(a,1x,a,i,a,i)') 'before thinning global', trim(instNameUniqueListWithHeader(1)), &
            --------------------^
            src/programs/prepcma.f90(1006): warning #6182: Fortran 2008 does not allow this edit descriptor.   [I]
                        write(*,'(a,1x,a,i,a,i)') 'after thinning global', trim(instNameUniqueListWithHeader(1)), &
            --------------------^

--- src/programs/prepcma.f90
+++ src/programs/prepcma.f90
@@ -113,7 +113,8 @@ program midas_prepcma
   type(struct_oti), pointer :: oti => null()
   real(kind=8) :: hx_dummy(1,1)
   integer :: ncmahdr, ncmahx, ncmabdy, ncmadim, nobsout, nbrpform
-  logical :: qcvar, numHeader, numBody
+  integer :: numHeader, numBody
+  logical :: qcvar
   character(len=7) :: resumeType
 
   ! number of pressure ranges used for the thinning of aircraft (and other) data:
@@ -299,8 +300,8 @@ program midas_prepcma
       ! thinning per instrument
       if (thinTovsPerInst) then
         call getTovsInstNameList(numTovsInstNameList,tovsInstNameList)
-        write(*,*) 'midas-prepcma: numTovsInstNameList=', numTovsInstNameList,', &
-                    tovsInstNameList(1:numTovsInstNameList)=', tovsInstNameList(1:numTovsInstNameList)
+        write(*,*) 'midas-prepcma: numTovsInstNameList=', numTovsInstNameList, &
+                    'tovsInstNameList(1:numTovsInstNameList)=', tovsInstNameList(1:numTovsInstNameList)
 
         do sensorIndex = 1, numTovsInstNameList
           tovsInstName = trim(tovsInstNameList(sensorIndex))
