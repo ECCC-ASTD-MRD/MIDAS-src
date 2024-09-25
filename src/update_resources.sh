bd7372f976b62c708ff293cef284dd11e5bcdcc9
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2024-09-25 13:36:17 +0000

    Issue #978: Simplify 'src/update_resources.sh' since now the file 'resources.def' will be rebuilt at each MIDAS build

--- src/update_resources.sh
+++ src/update_resources.sh
@@ -30,66 +30,33 @@ else
     typeset -r letkf_glb15km_memory_expected_value=11G
 fi
 
-
 typeset -r resources_file=${toplevel}/maestro/suites/midas_system_tests/resources/resources.def
-## If the resources file does not exist, then the user has not yet
-## run 'set_resources_def.sh' so we will call it from here
-#if [ ! -f "${resources_file}" ]; then
-    ${toplevel}/set_resources_def.sh
-#fi
+
+## Rebuild the 'resources.def' file by calling 'set_resources_def.sh'
+${toplevel}/set_resources_def.sh
 
 ## First, check if there is one and only one definition of the
 ## variable 'CHECK_RESULTS_CATCHUP' in the resources file
-#if [ "$(grep -c '^CHECK_RESULTS_CATCHUP=' ${resources_file})" -ne 1 ]; then
-#    echo "Found none or several definitions of the variable 'CHECK_RESULTS_CATCHUP' in '${resources_file}'"
-#    echo "We will not modify the resources file"
-#    exit
-#fi
-## Same thing with variable 'CLEAN_UNITTEST_CATCHUP'
-#if [ "$(grep -c '^CLEAN_UNITTEST_CATCHUP=' ${resources_file})" -ne 1 ]; then
-#    echo "Found none or several definitions of the variable 'CLEAN_UNITTEST_CATCHUP' in '${resources_file}'"
-#    echo "We will not modify the resources file"
-#    exit
-#fi
+if [ "$(grep -c '^CHECK_RESULTS_CATCHUP=' ${resources_file})" -ne 1 ]; then
+    echo "Found none or several definitions of the variable 'CHECK_RESULTS_CATCHUP' in '${resources_file}'"
+    echo "We will not modify the resources file"
+    exit
+fi
+# Same thing with variable 'CLEAN_UNITTEST_CATCHUP'
+if [ "$(grep -c '^CLEAN_UNITTEST_CATCHUP=' ${resources_file})" -ne 1 ]; then
+    echo "Found none or several definitions of the variable 'CLEAN_UNITTEST_CATCHUP' in '${resources_file}'"
+    echo "We will not modify the resources file"
+    exit
+fi
 
-## If the variables 'CHECK_RESULTS_CATCHUP', 'CLEAN_UNITTEST_CATCHUP',
-## 'LETKF_GLB15KM_NUM_THREADS' and 'LETKF_GLB15KM_MEMORY' all have
-## already the expected value, then we don't need to change the file.
-typeset -r check_result_catchup=$(awk -F= '/^CHECK_RESULTS_CATCHUP=/ {print $2}' ${resources_file})
-typeset -r clean_unittest_catchup=$(awk -F= '/^CLEAN_UNITTEST_CATCHUP=/ {print $2}' ${resources_file})
-typeset -r letkf_glb15km_num_threads=$(awk -F= '/^LETKF_GLB15KM_NUM_THREADS=/ {print $2}' ${resources_file})
-typeset -r letkf_glb15km_memory=$(awk -F= '/^LETKF_GLB15KM_MEMORY=/ {print $2}' ${resources_file})
-#if [ "${check_result_catchup}"   -eq "${check_result_catchup_expected_value}" -a \
-#     "${clean_unittest_catchup}" -eq "${clean_unittest_catchup_expected_value}" -a \
-#     "${letkf_glb15km_num_threads}" -eq "${letkf_glb15km_num_threads_expected_value}" -a \
-#     "${letkf_glb15km_memory}" = "${letkf_glb15km_memory_expected_value}" ]; then
-    ## echo "All variables 'CHECK_RESULTS_CATCHUP', 'CLEAN_UNITTEST_CATCHUP', 'LETKF_GLB15KM_NUM_THREADS' and 'LETKF_GLB15KM_MEMORY' are all already set"
-    ## echo "to ${check_result_catchup_expected_value} and ${clean_unittest_catchup_expected_value}"
-    ## echo "respectively in '${resources_file}'."
-    ## echo "We will not modify the resources file"
-#    exit
-#fi
+echo "Set 'CHECK_RESULTS_CATCHUP=${check_result_catchup_expected_value}' in ${resources_file}"
+sed -i "s/^CHECK_RESULTS_CATCHUP=[1-9]$/CHECK_RESULTS_CATCHUP=${check_result_catchup_expected_value}/" ${resources_file}
 
-## If 'CHECK_RESULTS_CATCHUP' is not set to 9, then update it
-#if [ "${check_result_catchup}" -ne "${check_result_catchup_expected_value}" ]; then
-    echo "Set 'CHECK_RESULTS_CATCHUP=${check_result_catchup_expected_value}' in ${resources_file}"
-    sed -i "s/^CHECK_RESULTS_CATCHUP=[1-9]$/CHECK_RESULTS_CATCHUP=${check_result_catchup_expected_value}/" ${resources_file}
-#fi
+echo "Set 'CLEAN_UNITTEST_CATCHUP=${clean_unittest_catchup_expected_value}' in ${resources_file}"
+sed -i "s/^CLEAN_UNITTEST_CATCHUP=[1-9]$/CLEAN_UNITTEST_CATCHUP=${clean_unittest_catchup_expected_value}/" ${resources_file}
 
-## If 'CLEAN_UNITTEST_CATCHUP' is not set to 9, then update it
-#if [ "${clean_unittest_catchup}" -ne "${clean_unittest_catchup_expected_value}" ]; then
-    echo "Set 'CLEAN_UNITTEST_CATCHUP=${clean_unittest_catchup_expected_value}' in ${resources_file}"
-    sed -i "s/^CLEAN_UNITTEST_CATCHUP=[1-9]$/CLEAN_UNITTEST_CATCHUP=${clean_unittest_catchup_expected_value}/" ${resources_file}
-#fi
+echo "Set 'LETKF_GLB15KM_NUM_THREADS=${letkf_glb15km_num_threads_expected_value}' in ${resources_file}"
+sed -i "s/^LETKF_GLB15KM_NUM_THREADS=[1-9]$/LETKF_GLB15KM_NUM_THREADS=${letkf_glb15km_num_threads_expected_value}/" ${resources_file}
 
-## If 'LETKF_GLB15KM_NUM_THREADS' is not set to 9, then update it
-#if [ "${letkf_glb15km_num_threads}" -ne "${letkf_glb15km_num_threads_expected_value}" ]; then
-    echo "Set 'LETKF_GLB15KM_NUM_THREADS=${letkf_glb15km_num_threads_expected_value}' in ${resources_file}"
-    sed -i "s/^LETKF_GLB15KM_NUM_THREADS=[1-9]$/LETKF_GLB15KM_NUM_THREADS=${letkf_glb15km_num_threads_expected_value}/" ${resources_file}
-#fi
-
-## If 'LETKF_GLB15KM_MEMORY' is not set to 9, then update it
-#if [ "${letkf_glb15km_memory}" != "${letkf_glb15km_memory_expected_value}" ]; then
-    echo "Set 'LETKF_GLB15KM_MEMORY=${letkf_glb15km_memory_expected_value}' in ${resources_file}"
-    sed -i "s/^LETKF_GLB15KM_MEMORY=[0-9]\+[MGT]$/LETKF_GLB15KM_MEMORY=${letkf_glb15km_memory_expected_value}/" ${resources_file}
-#fi
+echo "Set 'LETKF_GLB15KM_MEMORY=${letkf_glb15km_memory_expected_value}' in ${resources_file}"
+sed -i "s/^LETKF_GLB15KM_MEMORY=[0-9]\+[MGT]$/LETKF_GLB15KM_MEMORY=${letkf_glb15km_memory_expected_value}/" ${resources_file}
