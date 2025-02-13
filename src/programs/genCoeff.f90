f9ad770e98a2c331ddfa7442cb1c8577283199d5
Maziar Bani Shahabadi <maziar.banishahabadi@ec.gc.ca>
2025-02-13 15:17:08 +0000

    Issue #1044: Functionality to read obs err files and not set `obs_oer` in `genCoeff`.

--- src/programs/genCoeff.f90
+++ src/programs/genCoeff.f90
@@ -313,9 +313,16 @@ contains
     call msg_memUsage('midas-genCoeff')
 
     !
-    !- Initialize the observation error covariances
+    !- Initialize the observation error covariances or just read obs err ascii files.
     !
-    if (.not. bcs_mimicSatbcor) call oer_readAndSetObsErrors(obsSpaceData, varMode) ! IN
+    if (.not. bcs_mimicSatbcor) then
+      call oer_readAndSetObsErrors(obsSpaceData, varMode) ! IN
+    else
+      if (tvs_mwAllskyAssim) then
+        call oer_readAndSetObsErrors(obsSpaceData, varMode, setObsOer_opt=.false.) ! IN
+      end if
+    end if
+
     call msg_memUsage('midas-genCoeff')
 
   end subroutine gencoeff_setup
