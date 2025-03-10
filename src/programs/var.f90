2285f5364ead44523314a1ad3c524a28183ef2e0
Maziar Bani Shahabadi <maziar.banishahabadi@ec.gc.ca>
2025-03-10 16:36:52 +0000

    Issue #1053: Cosmetic change in `var` program.

--- src/programs/var.f90
+++ src/programs/var.f90
@@ -495,7 +495,7 @@ program midas_var
     end if
 
     ! Horizontally interpolate high-resolution stateVectorUpdate to trial columns
-    deallocInterpInfoNL = ( numOuterLoopIterations <= 1 )
+    deallocInterpInfoNL = ( numOuterLoopIterations == 1 )
     call inn_setupColumnsOnTrlLev( columnTrlOnTrlLev, obsSpaceData, hco_core, &
                                    stateVectorUpdateHighRes, &
                                    deallocInterpInfoNL_opt=deallocInterpInfoNL )
