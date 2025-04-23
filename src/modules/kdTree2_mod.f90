67f5374b91aa670bfe272231698b31da91e9bced
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2025-04-23 17:01:32 +0000

    Issue #1068: Replace 'forall' by a 'do' loop
    This is fixing this warning:
        FORALL construct and statement are obsolescent features in Fortran 2018.

--- src/modules/kdTree2_mod.f90
+++ src/modules/kdTree2_mod.f90
@@ -679,9 +679,9 @@ contains
       type(tree_node), pointer :: dummy => null()
       ! ..
       allocate (tp%ind(tp%n))
-      forall (j=1:tp%n)
+      do j = 1, tp%n
          tp%ind(j) = j
-      end forall
+      end do
       tp%root => build_tree_for_range(tp,1,tp%n, dummy)
     end subroutine build_tree
 
