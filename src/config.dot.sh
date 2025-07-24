7e3dca944e91551bae059768ba7627f46e57e385
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2025-07-24 16:02:09 +0000

    Issue #1100: Using a temporary 'perftools' compiled with Intel 2025.1.0

--- src/config.dot.sh
+++ src/config.dot.sh
@@ -160,7 +160,8 @@ echo "... loading /home/ant000/data/sitestore7/fs/ssm/eccc/cmd/cmda/libs/2025080
 . ssmuse-sh -d /home/ant000/data/sitestore7/fs/ssm/eccc/cmd/cmda/libs/20250808/${COMP_ARCH}
 
 echo "... loading main/opt/perftools/perftools-2.0/${COMP_ARCH}"
-. ssmuse-sh -x main/opt/perftools/perftools-2.0/${COMP_ARCH}
+#. ssmuse-sh -x main/opt/perftools/perftools-2.0/${COMP_ARCH}
+. ssmuse-sh -x /home/erv000/perftools
 
 echo "... loading /fs/homeu3/homeu3eb/erv000/rttov/rttov_v13/tmp/rttov13/2.0.1-3-g9ee0b5d"
 . ssmuse-sh -x /fs/homeu3/homeu3eb/erv000/rttov/rttov_v13/tmp/rttov13/2.0.1-3-g9ee0b5d
