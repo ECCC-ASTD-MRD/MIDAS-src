e4a4ad6d8e6493cb3009dbaa407f652d5768878c
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2025-09-05 20:00:35 +0000

    Issue #1100: Fix continuation lines for long OpenMP directives

--- src/modules/bMatrixEnsemble_mod.f90
+++ src/modules/bMatrixEnsemble_mod.f90
@@ -2438,8 +2438,8 @@ CONTAINS
 
       ensMemberAll_r4 => ens_getOneLev_r4(bEns(instanceIndex)%ensPerts(waveBandIndex),levIndex)
       !$OMP PARALLEL DO PRIVATE (latIndex,lonIndex,stepIndex, stepIndex2, stepIndex_amp, &
-           memberIndex,ensAmplitude_oneLev, ensAmplitude_oneLevM1, &
-           ensAmplitude_oneLevP1, ensAmplitude_MT)
+      !$omp&     memberIndex,ensAmplitude_oneLev, ensAmplitude_oneLevM1, &
+      !$omp&     ensAmplitude_oneLevP1, ensAmplitude_MT)
       do latIndex = bEns(instanceIndex)%myLatBeg, bEns(instanceIndex)%myLatEnd
         do lonIndex = bEns(instanceIndex)%myLonBeg, bEns(instanceIndex)%myLonEnd
 
