71f534d73eccc3903eef209818317214b704a824
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2025-10-07 17:11:06 +0000

    Issue #1100: Upgrade 'rpn/libs' and 'rpn/utils' to '20251001'

--- src/config.dot.sh
+++ src/config.dot.sh
@@ -151,13 +151,13 @@ HDF5_LIBS="hdf5hl_fortran hdf5_hl hdf5_fortran hdf5 z"
 
 ## for rmn, vgrid, rpncomm
 VGRID_LIBNAME="vgrid"
-echo "... loading eccc/mrd/rpn/libs/20250804"
-. r.load.dot eccc/mrd/rpn/libs/20250804
+echo "... loading eccc/mrd/rpn/libs/20251001"
+. r.load.dot eccc/mrd/rpn/libs/20251001
 echo "... loading hdf5"
 . ssmuse-sh -d main/opt/hdf5-netcdf4/parallel/intelmpi-2025.1.0/alllib/${COMP_ARCH}/01
 
-echo "... loading eccc/mrd/rpn/utils/20250804/burp-tools_20.0.12-${COMP_ARCH}_${ORDENV_PLAT}"
-. r.load.dot eccc/mrd/rpn/utils/20250804/burp-tools_20.0.12-${COMP_ARCH}_${ORDENV_PLAT}
+echo "... loading eccc/mrd/rpn/utils/20251001/burp-tools_20.0.13-${COMP_ARCH}_${ORDENV_PLAT}"
+. r.load.dot eccc/mrd/rpn/utils/20251001/burp-tools_20.0.13-${COMP_ARCH}_${ORDENV_PLAT}
 
 #echo "... loading eccc/cmd/cmda/libs/20250604-beta/${COMP_ARCH}"
 echo "... loading /home/ant000/data/sitestore7/fs/ssm/eccc/cmd/cmda/libs/20250804/${COMP_ARCH}"
