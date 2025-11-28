3e62463dfc97be9ca63a46de8acacfb28443dbae
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2025-11-28 19:56:35 +0000

    Issue #1130: Put the test name and the outlier message on the same line

--- maestro/suites/midas_system_tests/extractRunTime.sh
+++ maestro/suites/midas_system_tests/extractRunTime.sh
@@ -145,12 +145,9 @@ END {
         if [ "${findOutliers}" = yes ]; then
             outlier=$(printf "${__findRunTime_runtime__}" | grep '^[.0-9][.0-9]* seconds which is greater' || true)
             if [ -n "${outlier}" ]; then
-                printf "${outlier}\n" | sed 's/^/     /'
-                line=$(printf "${outlier}" | sed 's/^/     /' | sed 's/%/%%/g')
-                outliers="${outliers}
-${findRunTime_nodes%/*}
-${line}
-"
+                printf "${outlier}\n" | sed 's/^/\t/'
+                line=$(printf "${outlier}" | sed 's/^/\t/' | sed 's/%/%%/g')
+                outliers="${outliers}${findRunTime_nodes%/*} ${line}\n"
             else
                 echo "No outlier"
             fi
