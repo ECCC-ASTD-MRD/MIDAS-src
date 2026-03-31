40b8b908ad911b70523dbfc9ce52a8c7daefc2bf
Martin Deshaies-Jacques <martin.deshaies-jacques@ec.gc.ca>
2026-03-31 19:49:50 +0000

    Issue #1152: introduced user SSM for external libraries
    does not compile:
    ```
    Fatal Error: Cannot open module file ‘netcdf.mod’ for reading at (1): No
    such file or directory
    ```
    
    This error is related to the failing build of external libraries by
    `ssm/package_external_libs`.  This will be delt with in #1163

--- src/config.dot.sh
+++ src/config.dot.sh
@@ -128,9 +128,9 @@ if [ "${__fresh_build_directory}" != true -a "${__fresh_build_directory}" != fal
 fi
 
 if [ "${__compiler}" = list ]; then
-    echo "The compiler supported are 'inteloneapi-2022.1.2', 'inteloneapi-2025.1.0' and 'gnu-14.2.0'."
+    echo "The compiler supported are 'inteloneapi-2022.1.2', 'inteloneapi-2025.1.0', 'gnu-14.2.0' and 'gnu-15.2.0'."
     __run_cmake=stop
-elif [ "${__compiler}" != inteloneapi-2022.1.2 -a "${__compiler}" != gnu-14.2.0 -a "${__compiler}" != inteloneapi-2025.1.0 ]; then
+elif [ "${__compiler}" != inteloneapi-2022.1.2 -a "${__compiler}" != gnu-14.2.0 -a "${__compiler}" != gnu-15.2.0 -a "${__compiler}" != inteloneapi-2025.1.0 ]; then
     echo "config.dot.sh: The compiler '${__compiler}' is not supported.  Only 'inteloneapi-2022.1.2', 'inteloneapi-2025.1.0', 'gnu-14.2.0' are." >&2
     __status=false
     __run_cmake=stop
@@ -280,10 +280,16 @@ if [ "${__run_cmake}" != stop ]; then
 
         echo "... loading gnu 14.2.0 compiler"
         export MODULEPATH=/home/sidr000/modules:${MODULEPATH}
-        . r.load.dot rpn/code-tools/20251217/env/exp/gnu-14.2.0
+        . r.load.dot rpn/code-tools/20260219/env/exp/gnu-14.2.0
+    elif [ "${__compiler}" = gnu-15.2.0 ]; then
+        __original_ordenv_plat__=${ORDENV_PLAT}
+
+        echo "... loading gnu 15.2.0 compiler"
+        export MODULEPATH=/home/sidr000/modules:${MODULEPATH}
+        . r.load.dot rpn/code-tools/20260219/env/exp/gnu-15.2.0
     else
         echo "... loading rpn/code-tools/20251217/env/${__compiler}"
-        . r.load.dot rpn/code-tools/20251217/env/${__compiler}
+        . r.load.dot rpn/code-tools/20260219/env/${__compiler}
     fi
 
     echo "... loading eccc/mrd/rpn/libs/20260401-beta"
@@ -303,6 +309,19 @@ if [ "${__run_cmake}" != stop ]; then
 
         echo "... loading perftools, udfsqlite and f90sqlite for ${COMP_ARCH}"
         . r.load.dot /home/erv000/SSM/midas-external-libs/1.0-rc2/${COMP_ARCH}
+    elif [ "${__compiler}" = gnu-15.2.0 ]; then
+        ## This is needed for CMake to find the library
+        export f90sqlite_installdir=/home/martn/ssm/f90sqlite_1.5-gnu-15.2.0_rhel-9-amd64-64
+
+        ## Loading the compiler affects 'ORDENV_PLAT' and for the
+        ## rest, we need to use the original value.
+        export ORDENV_PLAT=${__original_ordenv_plat__}
+        unset __original_ordenv_plat__
+
+        echo "... loading perftools, udfsqlite and f90sqlite for ${COMP_ARCH}"
+        . ssmuse-sh -x /fs/homeu3/eccc/mrd/rpnad/mad001/ssm/perftools_2.1-gnu-15.2.0_rhel-9-amd64-64
+        . ssmuse-sh -x /fs/homeu3/eccc/mrd/rpnad/mad001/ssm/udfsqlite_1.19-gnu-15.2.0_rhel-9-amd64-64
+        . ssmuse-sh -x /fs/homeu3/eccc/mrd/rpnad/mad001/ssm/f90sqlite_1.5-gnu-15.2.0_rhel-9-amd64-64
     else
         echo "... loading eccc/cmd/cmda/libs/20260401-beta/${COMP_ARCH}"
         . ssmuse-sh -d eccc/cmd/cmda/libs/20260401-beta/${COMP_ARCH}
@@ -329,10 +348,15 @@ if [ "${__run_cmake}" != stop ]; then
     else
         __rttovdebug__=
     fi
-    export RTTOV_VERSION=2.1.0 ## This variable is used in '../CMakeLists.txt' for the script 'midas-config'
-    __rttov_path__=/fs/ssm/eccc/mrd/rpn/anl/rttov13/${RTTOV_VERSION}/${COMP_ARCH}${__rttovdebug__}
-    echo "... loading ${__rttov_path__}"
-    . r.load.dot ${__rttov_path__}
+    if [ "${__compiler}" = gnu-15.2.0 ]; then
+      echo "... loading /fs/homeu3/eccc/mrd/rpnad/mad001/ssm/rttov_13-gnu-15.2.0__rhel-9-amd64-64"
+      . ssmuse-sh -x /fs/homeu3/eccc/mrd/rpnad/mad001/ssm/rttov_13-gnu-15.2.0__rhel-9-amd64-64
+    else
+      export RTTOV_VERSION=2.1.0 ## This variable is used in '../CMakeLists.txt' for the script 'midas-config'
+      __rttov_path__=/fs/ssm/eccc/mrd/rpn/anl/rttov13/${RTTOV_VERSION}/${COMP_ARCH}${__rttovdebug__}
+      echo "... loading ${__rttov_path__}"
+      . r.load.dot ${__rttov_path__}
+    fi
 
     ## Why is this needed to 'gfortran' and not for Intel
     if [ "${__compiler}" = gnu-14.2.0 ]; then
