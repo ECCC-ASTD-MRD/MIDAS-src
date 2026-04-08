b5441225b7ada5273cd430640b044fa5a61e6ff9
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2026-04-08 14:08:19 +0000

    Issue #1158: Allow to configure the tests database for 'midas.createLamTemplateGrids.Abs'
    One can use the environment variable
    'CREATELAMGRIDS_UNITTEST_DATABASE' to point to another set of
    inputs/outputs for this test.

--- tools/createLamTemplateGrids/test.sh
+++ tools/createLamTemplateGrids/test.sh
@@ -4,9 +4,11 @@
 
 set -eo pipefail
 
+CREATELAMGRIDS_UNITTEST_DATABASE=${CREATELAMGRIDS_UNITTEST_DATABASE:-/home/sanl000/data/ppp7/UnitTests/midas/createLamTemplateGrids/0001}
+
 createLAMgrids_PGM=${1:-./midas.createLamTemplateGrids.Abs}
-inputFile=${2:-/home/sanl000/data/ppp7/UnitTests/midas/createLamTemplateGrids/0001/2026033106_006}
-referenceFile=${3:-/home/sanl000/data/ppp7/UnitTests/midas/createLamTemplateGrids/0001/analysisgrid}
+inputFile=${2:-${CREATELAMGRIDS_UNITTEST_DATABASE}/2026033106_006}
+referenceFile=${3:-${CREATELAMGRIDS_UNITTEST_DATABASE}/analysisgrid}
 
 echo "Testing program '${createLAMgrids_PGM}'"
 
@@ -16,7 +18,7 @@ echo "Testing program '${createLAMgrids_PGM}'"
 
 ${createLAMgrids_PGM} $inputFile -grd_ext_x 280 -grd_ext_y 280
 
-fstcomp -a ./analysisgrid -b $referenceFile -n 2>&1 | tee fstcomp.list  
+fstcomp -a ./analysisgrid -b $referenceFile -n 2>&1 | tee fstcomp.list
 
 cat fstcomp.list | grep -vE '^[*] [*] [*] | FSTCOMP |^   \*|^1|^c_fstopl option REDUCTION32|^ Debug TG= T| SONT EGAUX$' | grep -v "^  NOM    ETIKET           IP1            IP2       IP3   E-REL-MAX   E-REL-MOY   VAR-A        C-COR        MOY-A        BIAIS       E-MAX       E-MOY" | grep -v "^  \*\*   SKIPPING RECORD \"!!\", CAN'T COMPARE  \*\*$" | grep -v "PAS DE COMPARAISON" | grep -v "No comparison possible:" | awk 'NF>0 && $(NF-1)!="0.0000E+00"' | tee fstcomp.list.grep
 
