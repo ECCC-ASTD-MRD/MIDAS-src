3175c0706081d2811a73380f33beb2d61ecc4666
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2026-04-20 12:07:28 +0000

    Issue #1167: Start the refactoring of 'splitobs' by creating a new function 'splitobs_sql' in its own file.

---
+++ tools/splitobs/splitobs_sql.h
@@ -0,0 +1,6 @@
+#ifndef __INCLUDE_SPLITOBS_SQL_H__
+#define __INCLUDE_SPLITOBS_SQL_H__
+
+int splitobs_sql(options opt, gridtype grid, gridtype grid_gz);
+
+#endif /* #ifndef __INCLUDE_SPLITOBS_SQL_H__ */
