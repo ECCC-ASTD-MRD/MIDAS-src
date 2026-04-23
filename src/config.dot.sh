c0190467ea4a4c05a758266a3d1373202d7a5338
Martin Deshaies-Jacques <martin.deshaies-jacques@ec.gc.ca>
2026-04-23 19:40:32 +0000

    Issue #1152: compiling, debug mode, gfortran, local extlibs

--- src/config.dot.sh
+++ src/config.dot.sh
@@ -329,14 +329,16 @@ if [ "${__run_cmake}" != stop ]; then
 
     if [ "${MIDAS_COMPILE_ADD_DEBUG_OPTIONS:-no}" = yes ]; then
         __rttovdebug__=-debug
+        ## Temporary dev different ssm base for RTTOV path
+        __rttov_path_gnu__=/fs/site7/eccc/mrd/rpnad/erv000/midas/rttov_v13-17/tmp/rttov13/2.1.0-16-g10f9cad
     else
         __rttovdebug__=
+        __rttov_path_gnu__=/fs/site7/eccc/mrd/rpnad/erv000/midas/rttov_v13-17/tmp/rttov13/2.1.0-17-g93b9e11
     fi
     if [ "${__compiler}" = gnu-15.2.0 ]; then
-        __rttov_path__=/fs/site7/eccc/mrd/rpnad/erv000/midas/rttov_v13-17/tmp/rttov13/2.1.0-17-g93b9e11
-        echo "... loading  ${__rttov_path__}${__rttovdebug__}"
-        . ssmuse-sh -x ${__rttov_path__}${__rttovdebug__}
-        export rttov_INSTALLDIR=${__rttov_path__}${__rttovdebug__}
+        echo "... loading  ${__rttov_path_gnu__}${__rttovdebug__}"
+        . ssmuse-sh -x ${__rttov_path_gnu__}${__rttovdebug__}
+        export rttov_INSTALLDIR=${__rttov_path_gnu__}${__rttovdebug__}
     else
         export RTTOV_VERSION=2.1.0 ## This variable is used in '../CMakeLists.txt' for the script 'midas-config'
         __rttov_path__=/fs/ssm/eccc/mrd/rpn/anl/rttov13/${RTTOV_VERSION}/${COMP_ARCH}${__rttovdebug__}
