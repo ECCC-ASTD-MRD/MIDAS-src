3430a5dbb262513c716a50c6b04e109d9295515e
Martin Deshaies-Jacques <martin.deshaies-jacques@ec.gc.ca>
2026-05-25 10:32:43 -0400

    Issue #1152: still removing spurious subtree files

--- extlibs/libudfsqlite/src/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-d.compile -conly -o libudfsqlite-shared.so                 -src udfsqlite.c -librmn rmn       -libsys z   -debug
-d.compile -conly -o libudfsqlite.a -defines =-DSQLITE_CORE -src udfsqlite.c -librmn rmn       -libsys z m -debug
-
