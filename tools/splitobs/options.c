449aa15461f683eb130bf0f0db9e74a39057fba5
Ervig Lapalme <ervig.lapalme@ec.gc.ca>
2026-06-08 15:32:44 +0000

    Issue #1180: Issue 'WARNING' instead of 'ERROR' when reporting deprecated options '-burpin' and '-burpout'.

--- tools/splitobs/options.c
+++ tools/splitobs/options.c
@@ -98,7 +98,7 @@ int parseOptions(int argc, char** argv, optionsptr optptr, int* VERBOSE) {
         strcpy(optptr->obsout,argv[++i]);
       }
       else if (!strcmp(argv[i],BURPIN_OPTION)) { /* Alors, on donne le nom du fichier BURP input */
-        App_Log(APP_ERROR,"Fonction parseOptions: L'option %s a ete remplacee par %s\n", argv[i], OBSIN_OPTION);
+        App_Log(APP_WARNING,"Fonction parseOptions: L'option %s a ete remplacee par %s\n", argv[i], OBSIN_OPTION);
         if (i+1>=argc || argv[i+1][0]=='-') {
           App_Log(APP_ERROR,"Fonction parseOptions: L'option %s demande un argument\n", argv[i]);
           App_End(-1);exit(1);
@@ -106,7 +106,7 @@ int parseOptions(int argc, char** argv, optionsptr optptr, int* VERBOSE) {
         strcpy(optptr->obsin,argv[++i]);
       }
       else if (!strcmp(argv[i],BURPOUT_OPTION)) { /* Alors, on donne le nom du fichier BURP output */
-        App_Log(APP_ERROR,"Fonction parseOptions: L'option %s a ete remplacee par %s\n", argv[i], OBSOUT_OPTION);
+        App_Log(APP_WARNING,"Fonction parseOptions: L'option %s a ete remplacee par %s\n", argv[i], OBSOUT_OPTION);
         if (i+1>=argc || argv[i+1][0]=='-') {
           App_Log(APP_ERROR,"Fonction parseOptions: L'option %s demande un argument\n", argv[i]);
           App_End(-1);exit(1);
